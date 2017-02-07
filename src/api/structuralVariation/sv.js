058f3ff0d038f7a8658f7848e9cdead3a15acdb7
604bpierce <brandon@piercefamily.ca>
2017-02-07 22:08:21 +0000

    New - Structural Variant section of the report. Includes API interface, template, and controller
    git-svn-id: https://svn.bcgsc.ca/svn/seqdev/ipr-client/trunk@127096 ad02020f-2a26-4133-947e-1829317c1416

---
+++ src/api/structuralVariation/sv.js
@@ -0,0 +1,71 @@
+/*
+ * BCGSC - IPR-Client User API
+ *
+ * This API factory implements the IPR-API. Calls to and from the API are
+ * managed through this construct.
+ *
+ */
+app.factory('api.structuralVariation.sv', ['_', '$http', '$q', (_, $http, $q) => {
+
+  const api = 'http://localhost:8001' + '/api/1.0/POG';
+
+  let $sv = {};
+
+
+  $sv.all = (pog) => {
+
+    let deferred = $q.defer();
+
+    $http.get(api + '/' + pog + '/genomic/structuralVariation/sv').then(
+      (resp) => {
+        // Successful authentication
+        deferred.resolve(resp.data);
+      },
+      (error) => {
+        deferred.reject('Unable to retrieve');
+      }
+    );
+
+    return deferred.promise;
+
+  };
+
+  $sv.one = (pog, ident) => {
+    return {
+      update: (data) => {
+        let deferred = $q.defer();
+
+        $http.put(api + '/' + pog + '/genomic/structuralVariation/sv/' + ident, data).then(
+          (resp) => {
+            deferred.resolve(resp.data);
+          },
+          (error) => {
+            console.log('Unable to update APC', error);
+            deferred.reject('Unable to update');
+          }
+        );
+
+        return deferred.promise;
+      }
+    }
+  };
+
+  // Get alterations by specific type
+  $sv.getType = (pog, type) => {
+    let deferred = $q.defer();
+
+    $http.get(api + '/'  + pog + '/genomic/structuralVariation/sv/' + type).then(
+      (resp) => {
+        deferred.resolve(resp.data);
+      },
+      (error) => {
+        deferred.reject('Unable to retrieve requested structural variation', error);
+      }
+    );
+
+    return deferred.promise;
+  };
+
+  return $sv;
+
+}]);
---
+++ src/components/dashboard/report/genomic/structuralVariation/structuralVariation.js
@@ -0,0 +1,42 @@
+app.controller('controller.dashboard.report.genomic.structuralVariation',
+  ['_', '$q', '$scope', '$state', '$mdDialog', '$mdToast', 'api.pog', 'pog', 'images', 'svs', 'ms',
+    (_, $q, $scope, $state, $mdDialog, $mdToast, $pog, pog, images, svs, ms) => {
+
+      // Load Images into template
+      $scope.images = images;
+      $scope.pog = pog;
+      $scope.ms = ms;
+      $scope.StrucVars = {};
+
+      $scope.titleMap = {
+        clinical: 'Gene Fusions of Potential Clinical Relevance with Genome & Transcriptome Support',
+        nostic: 'Gene Fusions of Prognostic and Diagnostic Relevance',
+        biological: 'Gene Fusions with Biological Relevance',
+        fusionOmicSupport: 'Gene Fusions with Genome and Transcriptome Support'
+      };
+
+      let processSvs = (structVars) => {
+
+        let svs = {
+          clinical: [],
+          nostic: [],
+          biological: [],
+          fusionOmicSupport: []
+        };
+
+        // Run over mutations and group
+        _.forEach(structVars, (row, k) => {
+          if(!(row.svVariant in svs)) svs[row.svVariant] = [];
+          // Add to type
+          svs[row.svVariant].push(row);
+        });
+
+        // Set Small Mutations
+        $scope.StrucVars = svs;
+      };
+
+      processSvs(svs);
+
+    }
+  ]
+);
