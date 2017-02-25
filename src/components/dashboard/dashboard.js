d4142717ca94ef890b4dffb0cb6b0bdd940a9848
604bpierce <brandon@piercefamily.ca>
2017-02-25 00:21:25 +0000

    Fix - Inject username into dashboard scope.
    git-svn-id: https://svn.bcgsc.ca/svn/seqdev/ipr-client/trunk@127748 ad02020f-2a26-4133-947e-1829317c1416

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,5 +1,5 @@
-app.controller('controller.dashboard', ['_', '$scope', 'api.pog', 'api.image', (_, $scope, $pog, $image) => {
- 
- 
-  
+app.controller('controller.dashboard', ['_', '$scope', 'api.pog', 'api.image', 'user', (_, $scope, $pog, $image, user) => {
+
+  $scope.user = user;
+
 }]);
