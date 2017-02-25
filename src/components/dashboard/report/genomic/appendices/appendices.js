459cc7de51a37eec7d89df24f43a73c71f560ae5
604bpierce <brandon@piercefamily.ca>
2017-02-25 00:21:08 +0000

    Fix - TCGA Acronyms now implemented.
    git-svn-id: https://svn.bcgsc.ca/svn/seqdev/ipr-client/trunk@127747 ad02020f-2a26-4133-947e-1829317c1416

--- src/components/dashboard/report/genomic/appendices/appendices.js
+++ src/components/dashboard/report/genomic/appendices/appendices.js
@@ -1,8 +1,13 @@
 app.controller('controller.dashboard.report.genomic.appendices',
-  ['_', '$q', '$scope', 'api.pog', '$mdDialog', '$mdToast', 'pog',
-    (_, $q, $scope, $pog, $mdDialog, $mdToast, pog) => {
+  ['_', '$q', '$scope', 'api.pog', '$mdDialog', '$mdToast', 'pog', 'tcgaAcronyms',
+    (_, $q, $scope, $pog, $mdDialog, $mdToast, pog, tcga) => {
 
       $scope.pog = pog;
+      $scope.tcga = tcga;
+
+      $scope.hashClean = (i) => {
+        return i.replace('#','');
+      }
 
     }
   ]
