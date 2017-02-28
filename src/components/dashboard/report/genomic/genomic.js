da5b87c7c547be443d14042fc136ca1765a7cc85
604bpierce <brandon@piercefamily.ca>
2017-02-28 20:20:21 +0000

    New - Genomic Report Menus now built procedurally
    git-svn-id: https://svn.bcgsc.ca/svn/seqdev/ipr-client/trunk@127868 ad02020f-2a26-4133-947e-1829317c1416

--- src/components/dashboard/report/genomic/genomic.js
+++ src/components/dashboard/report/genomic/genomic.js
@@ -3,10 +3,88 @@ app.controller('controller.dashboard.report.genomic',
   (_, $q, $scope, $state, $pog, pog) => {
 
   $scope.pog = pog;
+
+  $scope.sections = [
+    {
+      name: 'Summary',
+      state: 'summary',
+      showChildren: false,
+      children: []
+    },
+    {
+      name: 'Analyst Comments',
+      state: 'analystComments',
+      showChildren: false,
+      children: []
+    },
+    {
+      name: 'Pathway Analysis',
+      state: 'pathwayAnalysis',
+      showChildren: false,
+      children: []
+    },
+    {
+      name: 'Detailed Genomic Analysis',
+      state: 'detailedGenomicAnalysis',
+      showChildren: false,
+      children: [
+        { name: 'Targeted Gene Report', state: 'detailedGenomicAnalysis'}
+      ]
+    },
+    {
+      name: 'Disease Specific Analysis',
+      state: 'diseaseSpecificAnalysis',
+      showChildren: false,
+      children: []
+    },
+    {
+      name: 'Somatic Mutations',
+      state: 'somaticMutations',
+      showChildren: false,
+      children: []
+    },
+    {
+      name: 'Copy Number Analyses',
+      state: 'copyNumberAnalyses',
+      showChildren: false,
+      children: []
+    },
+    {
+      name: 'Structural Variation',
+      state: 'structuralVariation',
+      showChildren: false,
+      children: []
+    },
+    {
+      name: 'Expression Analysis',
+      state: 'expressionAnalysis',
+      showChildren: false,
+      children: []
+    },
+    {
+      name: 'Appendices',
+      state: 'appendices',
+      showChildren: false,
+      children: []
+    },
+  ];
+
+    /**
+     * Check if the provided state is the current one
+     *
+     * @param state
+     * @returns {boolean}
+     */
+  $scope.activeSection = (section) => {
+    if($state.current.name.indexOf(section.state) > -1) {
+      return true;
+    }
+    return false;
+  };
+
   $scope.goToReportSection = (goto) => {
     
     $state.go('^.' + goto);
-    
-  }
+  };
   
 }]);
