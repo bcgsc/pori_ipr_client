06b241215a249ec2a4a910854001a2ae2515d0fb
604bpierce <brandon@piercefamily.ca>
2017-03-29 17:31:59 -0700

    New - Basic editor in place
    Can add/update drug target entries. Listing all types using new carded row format. #UGNE-1457

---
+++ src/components/dashboard/report/genomic/therapeutic/therapeutic.edit.js
@@ -0,0 +1,136 @@
+app.controller('controller.dashboard.report.genomic.therapeutic.edit',
+['_', '$q', '$scope', '$mdDialog', '$mdToast', 'api.complete', 'api.therapeuticOptions', 'api.knowledgebase', 'pog', 'entry', 'newEntry',
+(_, $q, scope, $mdDialog, $mdToast, $complete, $therapeutic, $kb, pog, entry, newEntry) => {
+
+  scope.tab = 'listing';
+  scope.entry = (entry || { target: [], targetContext: null, biomarker: [], notes: null });
+  scope.type = (entry) ? entry.type : newEntry;
+  scope.bioMarketContexts = [
+    'Homozygous Deletion',
+    'Copy Gain',
+    'Likely LoF',
+    'Structural Variant',
+    'Likely GoF',
+    'Outlier',
+    'Outlier Expression'
+  ];
+
+  scope.new = {
+    biomarkerContextValue: null,
+    biomarkerValue: null,
+  };
+
+  scope.removeBiomarkerContext = (marker, context) => {
+    delete scope.entry.biomarker[marker].context.splice(context,1);
+  };
+
+  scope.removeBiomarker = (marker) => {
+    delete scope.entry.biomarker.splice(marker,1);
+  };
+
+  scope.newBiomarkerContext = (marker) => {
+    if(scope.new.biomarkerContextValue === null) return;
+    scope.entry.biomarker[marker].context.push(scope.new.biomarkerContextValue); // Add new entry
+    scope.new.biomarkerContextValue = null; // Blank out!
+  };
+
+  scope.newBiomarker = () => {
+    if(scope.new.biomarkerValue === null) return;
+    scope.entry.biomarker.push({entry: scope.new.biomarkerValue, context: []});
+    scope.new.biomarkerValue = null;
+  };
+
+  // Need to chipify the geneVar entries
+  let targets = [];
+  if(entry !== null) {
+    _.forEach(entry.target, (e) => {
+      if(e !== null) targets.push({entry: e});
+    });
+  }
+
+  let biomarkers = [];
+  // Transform chip for auto complete
+  scope.transformChip = (geneVar) => {
+    // If it is an object, it's already a known chip
+    if (angular.isObject(geneVar)) return geneVar;
+
+    // Otherwise, create a new one
+    return { geneVar: geneVar, type: 'new' }
+  };
+
+  scope.geneVar = {};
+
+  // Auto-complete search filter
+  scope.geneVar.filter = (query) => {
+    let deferred = $q.defer();
+    if(query.length < 3) deferred.resolve([]);
+
+    if(query.length >= 3) {
+      $kb.genevar(query).then(
+        (entries) => {
+          deferred.resolve(entries);
+        },
+        (err) => {
+          console.log('Unable to search for entries', err);
+        }
+      );
+    }
+    return deferred.promise;
+  };
+
+  scope.geneTarget = {};
+
+  scope.geneTarget.filter = (query) => {
+    return $q((resolve, reject) => {
+      resolve([]);
+    });
+  };
+
+  // Check, clean, and prepare save object
+  scope.save = () => {
+
+    let newTherapeutic = angular.copy(scope.entry);
+
+    // De-objectify targets
+    let targets = [];
+    _.forEach(scope.entry.target, (v)=> {
+      targets.push(v.geneVar);
+    });
+
+    newTherapeutic.target = targets;
+
+    // Is this a new entry?
+    if(newEntry !== false) {
+
+      newTherapeutic.type = newEntry; // Set Type passed by creator
+
+      console.log(newTherapeutic);
+      // Save new entry
+      $therapeutic.create(pog.POGID, newTherapeutic).then(
+        (result) => {
+          // New entry created!
+          $mdDialog.hide({status: true, data: result, 'new': true});
+        },
+        (err) => {
+          console.log('Unable to create new entry!');
+        }
+      );
+    } else {
+      // Update existing entry
+      $therapeutic.one(pog.POGID).update(newTherapeutic.ident, newTherapeutic).then(
+        (result) => {
+          // New entry created!
+          $mdDialog.hide({status: true, data: result, 'new': false});
+        },
+        (err) => {
+          console.log('Unable to update entry!');
+        }
+      );
+    }
+  };
+
+  scope.cancel = () => {
+    $mdDialog.cancel();
+  }
+
+}]); // End controller
--- src/components/dashboard/report/genomic/therapeutic/therapeutic.js
+++ src/components/dashboard/report/genomic/therapeutic/therapeutic.js
@@ -2,8 +2,6 @@ app.controller('controller.dashboard.report.genomic.therapeutic',
 ['_', '$q', '$scope', '$state', '$mdDialog', '$mdToast', 'api.pog', 'pog', 'api.therapeuticOptions', 'therapeutic',
 (_, $q, $scope, $state, $mdDialog, $mdToast, $pog, pog, $therapeutic, therapeutic) => {
 
-  //$scope.therapeutic = therapeutic;
-
   $scope.therapeutic = {
     therapeutic: [],
     chemoresistance: []
@@ -12,13 +10,63 @@ app.controller('controller.dashboard.report.genomic.therapeutic',
   // Sort into groups
   let groupTherapeutics = () => {
     _.forEach(therapeutic, (v) => {
-      if(v.type === 'therapeutic') $scope.therapeutic.therapeutic.push(v);
+      if(v.type === 'therapeutic') {
+        let targets = [];
+        _.forEach(v.target, (e) => {
+          targets.push((angular.isObject(e)) ? e : {geneVar: e});
+        });
+        v.target = targets;
+        $scope.therapeutic.therapeutic.push(v);
+      }
       if(v.type === 'chemoresistance') $scope.therapeutic.chemoresistance.push(v);
     });
   };
 
   groupTherapeutics();
-  console.log($scope.therapeutic);
+
+  // Edit Therapeutic Targets
+  $scope.edit = ($event, entry) => {
+    $mdDialog.show({
+      targetEvent: $event,
+      clickOutsideToClose: false,
+      locals: {
+        newEntry: false,
+        entry: entry,
+        pog: pog
+      },
+      templateUrl: 'dashboard/report/genomic/therapeutic/therapeutic.edit.html',
+      controller: 'controller.dashboard.report.genomic.therapeutic.edit'
+    });
+  };
+
+  $scope.newEntry = ($event, type) => {
+
+    // Create new entry by type
+    $mdDialog.show({
+      targetEvent: $event,
+      clickOutsideToClose: false,
+      locals: {
+        newEntry: type,
+        entry: false,
+        pog: pog
+      },
+      templateUrl: 'dashboard/report/genomic/therapeutic/therapeutic.edit.html',
+      controller: 'controller.dashboard.report.genomic.therapeutic.edit'
+    }).then(
+      (result) => {
+        if(result.type === 'therapeutic') {
+          let targets = [];
+          _.forEach(result.target, (e) => {
+            targets.push((angular.isObject(e)) ? e : {geneVar: e});
+          });
+          result.target = targets;
+          $scope.therapeutic.therapeutic.push(result);
+        }
+        if(result.type === 'chemoresistance') $scope.therapeutic.chemoresistance.push(result);
+      }
+    );
+
+  };
 
 
 }]);
