032aeda181e070eeda6d1bd265ca6707ab967fce
604bpierce <brandon@piercefamily.ca>
2017-03-29 17:33:04 -0700

    New - API Interface for getting list of Gene/Variant entries from KB
    #UGNE-1457

--- src/api/knowledgebase.js
+++ src/api/knowledgebase.js
@@ -19,7 +19,7 @@ app.factory('api.knowledgebase', ['_', '$http', '$q', (_, $http, $q) => {
 
       $http.post(api + '/validate/events', {events_expression: input}).then(
         (result) => {
-          deferred.resolve(result);
+          deferred.resolve(result.data);
         },
         (err) => {
           deferred.reject(err);
@@ -30,5 +30,27 @@ app.factory('api.knowledgebase', ['_', '$http', '$q', (_, $http, $q) => {
     }
 
   };
+
+  /**
+   * Check for a gene or variant in KB & HUGO
+   *
+   * @param {string} query - Input string to search DB against
+   * @returns {Promise|array} - Resolves an array of text values
+   */
+  $kb.genevar = (query) => {
+    let deferred = $q.defer();
+
+    $http.get(api + '/genevar?query=' + query).then(
+      (result) =>{
+        deferred.resolve(result.data);
+      },
+      (err) => {
+        deferred.reject(err);
+      }
+    );
+
+    return deferred.promise;
+  };
+
   return $kb;
 }]);
