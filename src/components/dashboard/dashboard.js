5dd444f822404e54eb18e1bbcf42578965473f3d
604bpierce <brandon@piercefamily.ca>
2017-04-20 13:10:39 -0700

    New - Add events and references submenu items for Dashboard
    #UGNE-1490

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,9 +1,10 @@
 app.controller('controller.dashboard',
-  ['_', '$scope', 'api.pog', 'api.image', 'user', 'isAdmin',
-    (_, $scope, $pog, $image, user, isAdmin) => {
+  ['_', '$scope', '$state', 'api.pog', 'api.image', 'user', 'isAdmin',
+    (_, $scope, $state, $pog, $image, user, isAdmin) => {
 
       $scope.isAdmin = isAdmin;
       $scope.user = user;
+      $scope.$state = $state;
 
       $scope.maximized = true;
       $scope.toggle = () => {
