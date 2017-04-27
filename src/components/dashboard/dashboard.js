15dca0f0acf2ec755c3f96ecf587dec0b7d5ecba
604bpierce <brandon@piercefamily.ca>
2017-04-27 11:28:20 -0700

    New - Save sidebar toggle state to user settings on dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,13 +1,14 @@
 app.controller('controller.dashboard',
-  ['_', '$scope', '$state', 'api.pog', 'api.image', 'user', 'isAdmin',
-    (_, $scope, $state, $pog, $image, user, isAdmin) => {
+  ['_', '$scope', '$state', 'api.pog', 'api.image', '$userSettings', 'user', 'isAdmin',
+    (_, $scope, $state, $pog, $image, $userSettings, user, isAdmin) => {
 
       $scope.isAdmin = isAdmin;
       $scope.user = user;
       $scope.$state = $state;
 
-      $scope.maximized = true;
+      $scope.maximized = $userSettings.get('sideBarState');
       $scope.toggle = () => {
+        $userSettings.save('sideBarState', !$scope.maximized);
         $scope.maximized = !$scope.maximized;
       }
 
