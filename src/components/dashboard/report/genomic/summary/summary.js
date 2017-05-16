3c488d6f988af00052032061e595c5c2913edbda
604bpierce <brandon@piercefamily.ca>
2017-05-16 14:50:52 -0700

    Fix - Capture deletion format for small mutations

--- src/components/dashboard/report/genomic/summary/summary.js
+++ src/components/dashboard/report/genomic/summary/summary.js
@@ -33,7 +33,7 @@ app.controller('controller.dashboard.report.genomic.summary',
     let cnvs = ['copy gain', 'copy loss', 'amplification', 'Homozygous Loss', 'Homozygous Gain'];
 
     // Small Mutations
-    if (variant.geneVariant.match(/([A-z0-9]*)\s(\(\p\.[A-z]*[0-9]*[A-z]*\*?\))/g)) {
+    if (variant.geneVariant.match(/([A-z0-9]*)\s(\(\p\.[A-z]*[0-9]*[A-z_0-9]*\*?\))/g)) {
       variant.type = "smallMutation";
       return variant;
     }
