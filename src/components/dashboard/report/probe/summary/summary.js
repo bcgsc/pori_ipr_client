d8f72a66e3bd26dd1be01fd35e65087b3036d17f
604bpierce <brandon@piercefamily.ca>
2017-05-18 12:03:28 -0700

    New - Display UI for modifying genomic events in probe report
    #UGNE-1580

--- src/components/dashboard/report/probe/summary/summary.js
+++ src/components/dashboard/report/probe/summary/summary.js
@@ -1,8 +1,9 @@
 app.controller('controller.dashboard.report.probe.summary',
-['_', '$q', '$state', '$scope', 'api.pog', '$mdDialog', '$mdToast', 'pog', 'report', 'testInformation', 'genomicEvents', 'api.probe.signature', 'signature',
-(_, $q, $state, $scope, $pog, $mdDialog, $mdToast, pog, report, testInformation, genomicEvents, $signature, signature) => {
+['_', '$q', '$state', '$scope', 'api.pog', '$mdDialog', '$mdToast', 'pog', 'report', 'testInformation', 'genomicEvents', 'api.probe.signature', 'api.summary.patientInformation', 'api.summary.genomicEventsTherapeutic', 'signature',
+(_, $q, $state, $scope, $pog, $mdDialog, $mdToast, pog, report, testInformation, genomicEvents, $signature, $patientInformation, $get, signature) => {
 
   $scope.pog = pog;
+  $scope.pi = pog.patientInformation;
   $scope.report = report;
   $scope.testInformation = testInformation;
   $scope.genomicEvents = genomicEvents;
@@ -17,7 +18,7 @@ app.controller('controller.dashboard.report.probe.summary',
       clickOutToClose: false,
       controller: ['scope', (scope) => {
 
-        scope.pi = angular.copy($scope.report.pi); //
+        scope.pi = angular.copy($scope.report.patientInformation); //
 
         scope.cancel = () => {
           $mdDialog.cancel('No changes were saved.');
@@ -70,5 +71,49 @@ app.controller('controller.dashboard.report.probe.summary',
       }
     )
   };
+
+
+
+  // Update Patient Information
+  $scope.modifyEvent = ($event, event) => {
+
+    $mdDialog.show({
+      targetEvent: $event,
+      templateUrl: 'dashboard/report/probe/summary/summary.events.html',
+      clickOutToClose: false,
+      controller: ['scope', (scope) => {
+
+        scope.event = angular.copy(event);
+
+        scope.cancel = () => {
+          $mdDialog.cancel('No changes were saved.');
+        };
+
+        scope.update = () => {
+
+          // Send updated entry to API
+          $get.update($scope.pog.POGID, report.ident, event.ident, scope.event).then(
+            (result) => {
+              $mdDialog.hide({message: 'Entry has been updated', data: result});
+            },
+            (error) => {
+              alert('Unable to update. See console');
+              console.log(error);
+            }
+          );
+
+        }; // End update
+      }] // End controller
+
+    }).then((outcome) => {
+      if (outcome) $mdToast.show($mdToast.simple().textContent(outcome.message));
+      _.forEach($scope.genomicEvents, (e, i) => {
+        if(e.ident === outcome.data.ident) $scope.genomicEvents[i] = outcome.data;
+      });
+    }, (error) => {
+      $mdToast.show($mdToast.simple().textContent('No changes were saved.'));
+    });
+
+  }; // End edit tumour analysis
 
 }]);
