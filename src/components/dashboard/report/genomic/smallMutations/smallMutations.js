4104de4f96c962662a0b049bc82c8c4f13effb36
604bpierce <brandon@piercefamily.ca>
2017-08-10 16:45:05 -0700

    New - Renamed somatic mutations section of report to small mutations

---
+++ src/components/dashboard/report/genomic/smallMutations/smallMutations.js
@@ -0,0 +1,33 @@
+app.controller('controller.dashboard.report.genomic.smallMutations',
+['_', '$q', '$scope', '$state', '$mdDialog', '$mdToast', 'api.pog', 'api.somaticMutations.smallMutations', 'api.vardb', 'pog', 'ms', 'images', 'smallMutations', 'mutationSignature',
+(_, $q, $scope, $state, $mdDialog, $mdToast, $pog, $smallMutations, $vardb, pog, ms, images, smallMutations, mutationSignature) => {
+
+  // Load Images into template
+  $scope.images = images;
+  $scope.pog = pog;
+  $scope.smallMutations = {};
+  $scope.mutationSignature = mutationSignature;
+  $scope.ms = ms;
+
+  let processMutations = (muts) => {
+    let mutations = {
+      clinical: [],
+      nostic: [],
+      biological: [],
+      unknown: []
+    };
+
+    // Run over mutations and group
+    _.forEach(muts, (row, k) => {
+      if(!(row.mutationType in mutations)) mutations[row.mutationType] = [];
+      // Add to type
+      mutations[row.mutationType].push(row);
+    });
+
+    // Set Small Mutations
+    $scope.smallMutations = mutations;
+  };
+
+  processMutations(smallMutations);
+
+}]);
