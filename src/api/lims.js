42f46aa5859696b7d7f126c0fc92e1ba3b746c2f
604bpierce <brandon@piercefamily.ca>
2017-10-17 10:00:39 -0700

    New - Biopsy listing & detail New - API interfaces for LIMS and BioApps

--- src/api/lims.js
+++ src/api/lims.js
@@ -29,7 +29,7 @@ app.factory('api.lims', ['_', '$http', '$q', (_, $http, $q) => {
         method: 'get',
         headers: {
           'Accept': 'application/json',
-          'Authorization': undefined
+          'Authorization': 'Basic YnBpZXJjZTprNHRZcDNScnl+'
         },
         url: api + '/dev/elastic_search',
         params: opts.params
@@ -90,6 +90,44 @@ app.factory('api.lims', ['_', '$http', '$q', (_, $http, $q) => {
   
   
   /**
+   * Get source information from LIMS
+   *
+   * @param pogs
+   * @returns {*}
+   */
+  $lims.source = (pogs) => {
+    return $q((resolve, reject) => {
+  
+      // Convert string pogid to array
+      if(typeof pogs === 'string') {
+        pogs = [pogs];
+      }
+  
+      let body = {
+        filters: {
+          op: "in",
+          content: {
+            field: "participant_study_id",
+            value: pogs
+          }
+        }
+      };
+      
+      $http.post('https://lims16.bcgsc.ca/alpha/limsapi/source', body, {headers: {Authorization: 'Basic YnBpZXJjZTprNHRZcDNScnl+'}}).then(
+        (result) => {
+          resolve(result.data);
+        })
+        .catch((err) => {
+          console.log('Failed to get LIMS sample result', err);
+          reject(err);
+        });
+      
+    
+    });
+  };
+  
+  
+  /**
    * Get Library information from LIMS
    *
    * @param {array} names - Names of libraries to look up
@@ -118,6 +156,55 @@ app.factory('api.lims', ['_', '$http', '$q', (_, $http, $q) => {
         })
         .catch((err) => {
           console.log('Failed to get LIMS library result', err);
+          reject(err);
+        });
+      
+    
+    });
+  };
+  
+  
+  /**
+   * Get Illumina Sequencing Run information from LIMS
+   *
+   * @param {array} libraries - Libraries to look up
+   * @returns {*}
+   */
+  $lims.illumina_run = (libraries) => {
+    return $q((resolve, reject) => {
+  
+      if(typeof names === 'string') {
+        libraries = [libraries];
+      }
+  
+      let body = {
+        filters: {
+          op: "or",
+          content: [
+            {
+              op: "in",
+              content: {
+                field: "library",
+                value: libraries
+              }
+            },
+            {
+              op: "in",
+              content: {
+                field: "multiplex_library",
+                value: libraries
+              }
+            }
+          ]
+        }
+      };
+      
+      $http.post('https://lims16.bcgsc.ca/alpha/limsapi/illumina_run', body, {headers: {Authorization: 'Basic YnBpZXJjZTprNHRZcDNScnl+'}}).then(
+        (result) => {
+          resolve(result.data);
+        })
+        .catch((err) => {
+          console.log('Failed to get LIMS Illumina run result', err);
           reject(err);
         });
       
