f1eefeb96cb6d0357804f56f754577c5e290cf5c
604bpierce <brandon@piercefamily.ca>
2017-11-03 13:44:00 -0700

    Fix - Hotfix for mutation summary barplots and density graph display on interactive and print reports. Fix - If no value for mutation summary table, display 'na'

--- src/components/print/report/genomic/sections/structuralVariants/structuralVariants.js
+++ src/components/print/report/genomic/sections/structuralVariants/structuralVariants.js
@@ -1,6 +1,6 @@
 app.controller('controller.print.POG.report.genomic.structuralVariants',
-['_', '$scope', '$sce', 'pog', 'report', 'images', 'ms', 'svs',
-(_, $scope, $sce, pog, report, images, ms, svs) => {
+['_', '$scope', '$sce', 'pog', 'report', 'images', 'ms', 'svs', 'mutationSummaryImages',
+(_, $scope, $sce, pog, report, images, ms, svs, mutationSummaryImages) => {
 
   // Data
   $scope.report = report;
@@ -39,7 +39,100 @@ app.controller('controller.print.POG.report.genomic.structuralVariants',
     // Set Small Mutations
     $scope.StrucVars = svs;
   };
+  
+  
+  let pickCompatator = () => {
+    let search = _.find(ms, {comparator: report.tumourAnalysis.diseaseExpressionComparator});
+    
+    if(!search) search = _.find(ms, {comparator: 'average'});
+    
+    $scope.ms = search;
+  };
+  
+  let processMutationSummaryImages = (images) => {
+    
+    let ssorted = {
+      barplot: {
+        indel: [],
+        snv: [],
+        sv: [],
+      },
+      densityPlot: {
+        indel: [],
+        snv: [],
+        sv: [],
+      },
+      legend: {
+        indel_snv: [],
+        sv: [],
+      }
+    };
+    
+    let sorted = {
+      comparators: [],
+      indel: {
+        barplot: [],
+        densityPlot: [],
+      },
+      snv: {
+        barplot: [],
+        densityPlot: [],
+      },
+      sv: {
+        barplot: [],
+        densityPlot: []
+      },
+      legend: {
+        snv_indel: [],
+        sv: null
+      }
+    };
+    
+    console.log(images);
+    
+    _.forEach(images, (img) => {
+      
+      let pieces = img.key.split('.');
+      img.comparator = pieces[2] || null;
+      
+      if(img.comparator && !_.find(sorted.comparators, {name: img.comparator})) sorted.comparators.push({name: img.comparator, visible: false});
+      
+      if(pieces[1].indexOf('barplot_indel') > -1) sorted.indel.barplot.push(img);
+      if(pieces[1].indexOf('barplot_snv') > -1) sorted.snv.barplot.push(img);
+      
+      if(pieces[1].indexOf('barplot_sv') > -1) sorted.sv.barplot.push(img);
+      
+      if(pieces[1].indexOf('density_plot_indel') > -1) sorted.indel.densityPlot.push(img);
+      if(pieces[1].indexOf('density_plot_snv') > -1) sorted.snv.densityPlot.push(img);
+      
+      if(pieces[1].indexOf('density_plot_sv') > -1) sorted.sv.densityPlot.push(img);
+      
+      if(pieces[1].indexOf('legend_snv_indel') > -1) sorted.legend.snv_indel.push(img);
+      if(pieces[1].indexOf('legend_sv') > -1) sorted.legend.sv = img;
+      
+    });
+    
+    $scope.mutationSummaryImages = sorted;
+    
+  };
+  
+  /**
+   * Retrieve specific mutation summary image
+   *
+   * @param {string} graph - The type of graph image to be retrieved (barplot, density graph, legend)
+   * @param {string} type - The type of analysis (snv, indel, sv)
+   * @param {string} comparator - OPTIONAL The comparator to be picked
+   *
+   * @returns
+   */
+  $scope.getMutationSummaryImage = (graph, type, comparator=null) => {
+    
+    return _.find($scope.mutationSummaryImages[type][graph], {comparator: comparator});
+    
+  };
 
   processSvs(svs);
+  pickCompatator();
+  processMutationSummaryImages(mutationSummaryImages);
 
 }]);
