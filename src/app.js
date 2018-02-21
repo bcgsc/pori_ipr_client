11261cc7c4d89df07fc2077b6965bbda888d586b
Nikki Martin <nikjean.martin@gmail.com>
2018-02-21 10:58:26 -0800

    Fix - Switching primary blue to gsc logo blue

--- src/app.js
+++ src/app.js
@@ -14,7 +14,17 @@ const app = angular.module('bcgscIPR', [
 	'chart.js',
 	'btford.socket-io',
 	'ngMessages',
-]);
+])
+.config(function($mdThemingProvider) {
+	let gscBlueMap = $mdThemingProvider.extendPalette('indigo' , {
+		'500': 'rgb(38, 50, 140)'
+	});
+
+	$mdThemingProvider.definePalette('gscBlue', gscBlueMap);
+
+	$mdThemingProvider.theme('default').primaryPalette('gscBlue');
+	
+});
 
 // Register HTTP Error Handler
 app.run(httpErrorHandler);
