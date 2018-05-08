f89dabf5cfe44372cdd414ab8e09fd23277cfeaf
Nikki Martin <nikjean.martin@gmail.com>
2018-05-08 15:30:09 -0700

    New - adding in dialog box to display info about probe reports

--- src/components/dashboard/reports/probe/probe.js
+++ src/components/dashboard/reports/probe/probe.js
@@ -116,13 +116,57 @@ app.controller('controller.dashboard.reports.probe', ['_', '$q', '$scope', 'api.
 
   // Show Dialog with searching tips
   $scope.showFilterTips = ($event) => {
+    let content = "The search bar can filter the listing of POGs using a number of special terms. ";
+    content    += "<ul><li>Filter by tumour content: <code>tc>50 tc<40 tc=35</code></li>";
+    content    += "<li>Filter by POG: <code>pog544</code></li><li>By tumour type: <code>brca</code></li>";
+    content    += "<li>By ploidy: <code>diploid</code></li>";
+    content    += "<li>By user involved: <code>bpierce</code>, <code>Brandon</code></li>";
+    content    += "<li>By disease: <code>melanoma</code></li></ul>";
 
     let alert = $mdDialog.show(
       $mdDialog.alert()
         .clickOutsideToClose(true)
         .title('POG Searching Tips')
-        .htmlContent("The search bar can filter the listing of POGs using a number of special terms. <ul><li>Filter by tumour content: <code>tc>50 tc<40 tc=35</code></li><li>Filter by POG: <code>pog544</code></li><li>By tumour type: <code>brca</code></li><li>By ploidy: <code>diploid</code></li><li>By user involved: <code>bpierce</code>, <code>Brandon</code></li> <li>By disease: <code>melanoma</code></li> </ul>")
-        .ok('Got it!')
+        .htmlContent(content)
+        .ok('Close')
+        .targetEvent($event)
+    );
+
+  };
+
+  $scope.showProbeDescription = ($event) => {
+    let content = "<h4>The Report:</h4>";
+    content    += "<p>The Targeted Gene Report (TGR) provides results from a rapid analysis pipeline designed to identify ";
+    content    += "specific somatic alterations in a select set of cancer-associated genes and gene fusion events. ";
+    content    += "This rapid analysis is not a complete description of aberrations present in the tumour genome. ";
+    content    += "The absence of a specific mutation in this report is not a guarantee that the mutation is not present in the patient's tumour. ";
+    content    += "Germline variants are not included in this report.</p>";
+
+    content    += "<h4>Test Method:</h4>";
+    content    += "<p>In the TGR, whole genome and whole transcriptome sequence reads are computationally queried to identify events matching ";
+    content    += "a specific list of known cancer-related aberrations. A subset of genes is examined for specific events which include, ";
+    content    += "but are not limited to, individual gene mutations such as hotspot mutations in KRAS, BRAF and PIK3CA, ";
+    content    += "and gene-pair fusions such as BCR-ABL1, EML4-ALK, and CCDC6-RET. ";
+    content    += "Genome and transcriptome sequence data are queried for individual gene mutations; only transcriptome data are queried for gene fusion events.</p>";
+
+    content    += "<h4>Reporting of information of Potential Clinical Relevance:</h4>";
+    content    += "<p>The TGR incorporates results from published peer-reviewed studies and other publicly available information through our in-house Knowledgebase - ";
+    content    += "a curated database of cancer-associated genes and genomic alterations. ";
+    content    += "Reported associations may include those of potential biological, diagnostic, prognostic and therapeutic significance. ";
+    content    += "Therapeutic associations of potential clinical benefit (or potential lack of clinical benefit) are derived from public data and are not independently verified.</p>";
+
+    content    += "<p>This report will generally be followed by a final report, "
+    content    += "which will provide a more comprehensive description of both previously observed and novel aberrations.</p>";
+
+    content    += "<p>The TGR is developed by Canada's Michael Smith Genome Sciences Centre, part of the British Columbia Cancer Agency. ";
+    content    += "Contents should be regarded as purely investigational and are intended for research purposes only.</p>";
+
+    let alert = $mdDialog.show(
+      $mdDialog.alert()
+        .clickOutsideToClose(true)
+        .title('About Probe (Targeted Gene) Reports')
+        .htmlContent(content)
+        .ok('Close')
         .targetEvent($event)
     );
 
