bbca2005a8b644b8c83f3286091db5a2bca8c2ba
Anna Davies <adavies@bcgsc.ca>
2019-01-18 14:39:35 -0800

    Fixed more await issues with the sidebar state

--- webpack/app/common/sidebar/sidebar.component.js
+++ webpack/app/common/sidebar/sidebar.component.js
@@ -13,13 +13,14 @@ class SidebarComponent {
   async $onInit() {
     const pages = ['analyses', 'tracking', 'report', 'genomic_report',
       'probe_report', 'germline', 'knowledgebase'];
-    pages.forEach((page) => {
-      this.pageAccess[page] = this.AclService.checkResource(page);
+    pages.forEach(async (page) => {
+      this.pageAccess[page] = await this.AclService.checkResource(page);
     });
     this.maximized = await this.UserSettingsService.get('sideBarState');
     this.$rootScope.$on('sidebarToggle', async () => {
       await this.UserSettingsService.save('sideBarState', !this.maximized);
       this.maximized = !this.maximized;
+      this.$rootScope.$digest();
     });
   }
 }
