187d56b6e0f80600377017690a474fbdbbe2aa70
Anna Davies <adavies@bcgsc.ca>
2019-03-15 14:10:21 -0700

    Remove alteration service, fix probe logic to match genomic logic

--- webpack/app/components/report-listing/probe/probe.component.js
+++ webpack/app/components/report-listing/probe/probe.component.js
@@ -87,6 +87,7 @@ class ProbeComponent {
     this.$scope.$digest();
   }
 
+
   /* eslint-disable-next-line class-methods-use-this */
   searchPogs(state, query) { // This should be reworked, dunno what's going on here really
     return (report) => {
@@ -97,22 +98,22 @@ class ProbeComponent {
         result = true;
       }
       // Run over each split by space
-      query.split(' ').forEach((q) => {
+      query.split(' ').forEach((queryEntry) => {
         if (report.state !== state) {
           result = false;
           return null;
         }
-        if (q.length === 0) {
+        if (queryEntry.length === 0) {
           result = true;
           return null;
         }
         // Pog ID?
-        if (report.analysis.pog.POGID.toLowerCase().includes(q.toLowerCase())) {
+        if (report.analysis.pog.POGID.toLowerCase().includes(queryEntry.toLowerCase())) {
           result = true;
         }
         // Tumour Type
         if (report.patientInformation && report.patientInformation.tumourType
-          && report.patientInformation.tumourType.toLowerCase().includes(q.toLowerCase())) {
+          && report.patientInformation.tumourType.toLowerCase().includes(queryEntry.toLowerCase())) {
           result = true;
         }
         // Tumour Type & Ploidy Model
@@ -120,40 +121,37 @@ class ProbeComponent {
           return null;
         }
         if (report.tumourAnalysis && report.tumourAnalysis.ploidy
-          && report.tumourAnalysis.ploidy.toLowerCase().includes(q.toLowerCase())) {
+          && report.tumourAnalysis.ploidy.toLowerCase().includes(queryEntry.toLowerCase())) {
           result = true;
         }
         // Comparators
         if (report.tumourAnalysis && report.tumourAnalysis.diseaseExpressionComparator
-          && report.tumourAnalysis.diseaseExpressionComparator.toLowerCase().includes(q.toLowerCase())) {
+          && report.tumourAnalysis.diseaseExpressionComparator.toLowerCase().includes(queryEntry.toLowerCase())) {
           result = true; // Disease
         }
         if (report.tumourAnalysis && report.tumourAnalysis.normalExpressionComparator
-          && report.tumourAnalysis.normalExpressionComparator.toLowerCase().includes(q.toLowerCase())) {
+          && report.tumourAnalysis.normalExpressionComparator.toLowerCase().includes(queryEntry.toLowerCase())) {
           result = true; // Normal
         }
         // TC Search TODO: Cleanup to single line using regex. Proof of concept/do they want this?
-        if (q.toLowerCase().includes('tc>')
-          && report.tumourAnalysis.tumourContent > parseInt(q.split('>').slice(-1).pop(), 10)) {
+        if (queryEntry.toLowerCase().includes('tc>')
+          && report.tumourAnalysis.tumourContent > parseInt(queryEntry.split('>').pop(), 10)) {
           result = true;
         }
-        if (q.toLowerCase().includes('tc<')
-          && report.tumourAnalysis.tumourContent < parseInt(q.split('<').slice(-1).pop(), 10)) {
+        if (queryEntry.toLowerCase().includes('tc<')
+          && report.tumourAnalysis.tumourContent < parseInt(queryEntry.split('<').pop(), 10)) {
           result = true;
         }
-        if (q.toLowerCase().includes('tc=')
-          && report.tumourAnalysis.tumourContent === parseInt(q.split('=').slice(-1).pop(), 10)) {
+        if (queryEntry.toLowerCase().includes('tc=')
+          && report.tumourAnalysis.tumourContent === parseInt(queryEntry.split('=').pop(), 10)) {
           result = true;
         }
         // Search Users
-        report.users.forEach((p) => {
-          if (p.user.firstName.toLowerCase().includes(q.toLowerCase())) {
-            result = true;
-          }
-          if (p.user.lastName.toLowerCase().includes(q.toLowerCase())) {
-            result = true;
-          }
-          if (p.user.username.toLowerCase().includes(q.toLowerCase())) {
+        report.users.forEach((userEntry) => {
+          if (userEntry.user.firstName.toLowerCase().includes(queryEntry.toLowerCase())
+            || userEntry.user.lastName.toLowerCase().includes(queryEntry.toLowerCase())
+            || userEntry.user.username.toLowerCase().includes(queryEntry.toLowerCase())
+          ) {
             result = true;
           }
         });
@@ -165,8 +163,8 @@ class ProbeComponent {
   // Show Dialog with searching tips
   showFilterTips($event) {
     let content = 'The search bar can filter the listing of POGs using a number of special terms. ';
-    content    += '<li>Filter by POG: <code>pog544</code></li>';
-    content    += '<li>By disease: <code>melanoma</code></li></ul>';
+    content += '<li>Filter by POG: <code>pog544</code></li>';
+    content += '<li>By disease: <code>melanoma</code></li></ul>';
 
     this.$mdDialog.show(this.$mdDialog.alert()
       .clickOutsideToClose(true)
@@ -178,44 +176,44 @@ class ProbeComponent {
 
   showProbeDescription($event) {
     let content = '<h4>The Report</h4>';
-    content    += '<p>The Targeted Gene Report (TGR) provides results from a rapid analysis pipeline designed to identify ';
-    content    += 'specific somatic alterations in a select set of cancer-associated genes and gene fusion events. ';
-    content    += 'This rapid analysis is not a complete description of aberrations present in the tumour genome. ';
-    content    += 'The absence of a specific mutation in this report is not a guarantee that the mutation is not present in the patient\'s tumour. ';
-    content    += 'Germline variants are not included in this report.</p>';
-    content    += '<hr>';
+    content += '<p>The Targeted Gene Report (TGR) provides results from a rapid analysis pipeline designed to identify ';
+    content += 'specific somatic alterations in a select set of cancer-associated genes and gene fusion events. ';
+    content += 'This rapid analysis is not a complete description of aberrations present in the tumour genome. ';
+    content += 'The absence of a specific mutation in this report is not a guarantee that the mutation is not present in the patient\'s tumour. ';
+    content += 'Germline variants are not included in this report.</p>';
+    content += '<hr>';
 
-    content    += '<h4>Test Method</h4>';
-    content    += '<p>In the TGR, whole genome and whole transcriptome sequence reads are computationally queried to identify events matching ';
-    content    += 'a specific list of known cancer-related aberrations. A subset of genes is examined for specific events which include, ';
-    content    += 'but are not limited to, individual gene mutations such as hotspot mutations in KRAS, BRAF and PIK3CA, ';
-    content    += 'and gene-pair fusions such as BCR-ABL1, EML4-ALK, and CCDC6-RET. ';
-    content    += 'Genome and transcriptome sequence data are queried for individual gene mutations; only transcriptome data are queried for gene fusion events.</p>';
-    content    += '<hr>';
+    content += '<h4>Test Method</h4>';
+    content += '<p>In the TGR, whole genome and whole transcriptome sequence reads are computationally queried to identify events matching ';
+    content += 'a specific list of known cancer-related aberrations. A subset of genes is examined for specific events which include, ';
+    content += 'but are not limited to, individual gene mutations such as hotspot mutations in KRAS, BRAF and PIK3CA, ';
+    content += 'and gene-pair fusions such as BCR-ABL1, EML4-ALK, and CCDC6-RET. ';
+    content += 'Genome and transcriptome sequence data are queried for individual gene mutations; only transcriptome data are queried for gene fusion events.</p>';
+    content += '<hr>';
 
-    content    += '<h4>Reporting of information of Potential Clinical Relevance</h4>';
-    content    += '<p>The TGR incorporates results from published peer-reviewed studies and other publicly available information through our in-house Knowledgebase - ';
-    content    += 'a curated database of cancer-associated genes and genomic alterations. ';
-    content    += 'Reported associations may include those of potential biological, diagnostic, prognostic and therapeutic significance. ';
-    content    += 'Therapeutic associations of potential clinical benefit (or potential lack of clinical benefit) are derived from public data and are not independently verified.</p>';
+    content += '<h4>Reporting of information of Potential Clinical Relevance</h4>';
+    content += '<p>The TGR incorporates results from published peer-reviewed studies and other publicly available information through our in-house Knowledgebase - ';
+    content += 'a curated database of cancer-associated genes and genomic alterations. ';
+    content += 'Reported associations may include those of potential biological, diagnostic, prognostic and therapeutic significance. ';
+    content += 'Therapeutic associations of potential clinical benefit (or potential lack of clinical benefit) are derived from public data and are not independently verified.</p>';
 
-    content    += '<p>This report will generally be followed by a final report, '
-    content    += 'which will provide a more comprehensive description of both previously observed and novel aberrations.</p>';
-    content    += '<hr>';
+    content += '<p>This report will generally be followed by a final report, ';
+    content += 'which will provide a more comprehensive description of both previously observed and novel aberrations.</p>';
+    content += '<hr>';
 
     if (this.isExternalMode) {
-      content    += '<h4>Field Descriptions</h4>';
-      content    += '<p>Patient: Study identification code</p>';
-      content    += '<p>Alternate Identifier: Alternative study identifier if enrolled in another genomics study (e.g. COMPARISON or PROFYLE IDs)';
-      content    += '<p>Disease: Primary diagnosis</p>';
-      content    += '<p>Physician: Most responsible clinician for receiving the genomic report</p>';
-      content    += '<p>Age: Status at enrollment</p>';
-      content    += '<p>Status: Status of the report</p>';
-      content    += '<hr>';
+      content += '<h4>Field Descriptions</h4>';
+      content += '<p>Patient: Study identification code</p>';
+      content += '<p>Alternate Identifier: Alternative study identifier if enrolled in another genomics study (e.g. COMPARISON or PROFYLE IDs)';
+      content += '<p>Disease: Primary diagnosis</p>';
+      content += '<p>Physician: Most responsible clinician for receiving the genomic report</p>';
+      content += '<p>Age: Status at enrollment</p>';
+      content += '<p>Status: Status of the report</p>';
+      content += '<hr>';
     }
 
-    content    += '<p>The TGR is developed by Canada\'s Michael Smith Genome Sciences Centre, part of the British Columbia Cancer Agency. ';
-    content    += 'Contents should be regarded as purely investigational and are intended for research purposes only.</p>';
+    content += '<p>The TGR is developed by Canada\'s Michael Smith Genome Sciences Centre, part of the British Columbia Cancer Agency. ';
+    content += 'Contents should be regarded as purely investigational and are intended for research purposes only.</p>';
 
     this.$mdDialog.show(this.$mdDialog.alert()
       .clickOutsideToClose(true)
