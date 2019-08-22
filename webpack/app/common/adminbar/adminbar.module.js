ae1b754deede2179f4b05945e46a3e1f9636727e
Anna Davies <adavies@bcgsc.ca>
2019-08-22 16:20:39 -0700

    Fixed admin page... enough for it to be usable

--- webpack/app/common/adminbar/adminbar.module.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import AdminbarComponent from './adminbar.component';
-
-angular.module('adminbar', []);
-
-export default angular.module('adminbar')
-  .component('adminbar', AdminbarComponent)
-  .name;
