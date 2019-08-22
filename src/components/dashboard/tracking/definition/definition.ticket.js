41111a618f7c42c0ecf7f5e8a65e20c5355ff647
Anna Davies <adavies@bcgsc.ca>
2019-08-22 16:22:52 -0700

    Delete remaining legacy files

--- src/components/dashboard/tracking/definition/definition.ticket.js
+++
@@ -1,7 +0,0 @@
-app.controller('controller.dashboard.tracking.definition.ticket',
-['$scope', '_', '$interpolate', '$mdDialog', '$mdToast', 'api.jira', 'definition',
-($scope, _, $interpolate, $mdDialog, $mdToast, $jira, definition) => {
-  
-
-
-}]);
