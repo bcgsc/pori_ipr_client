2dfc5d6a849fd64bcabdf4413ca8eb4516e23cb9
Anna Davies <adavies@bcgsc.ca>
2019-10-29 12:47:43 -0700

    PR#340 Add print dialog on page load

--- app/components/print/print.component.js
+++ app/components/print/print.component.js
@@ -6,8 +6,22 @@ const bindings = {
   report: '<',
 };
 
+class PrintComponent {
+  /* @ngInject */
+  constructor($window) {
+    this.$window = $window;
+  }
+
+  $onInit() {
+    // This looks odd but it's the only way to simulate a postLink lifecycle method
+    // Where all the template is bound to variables
+    angular.element(() => this.$window.print());
+  }
+}
+
 
 export default {
   template,
   bindings,
+  controller: PrintComponent,
 };
