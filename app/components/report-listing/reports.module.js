117dc57a98fa27fcb1d4c77ac88187c4c6eceec5
Anna Davies <adavies@bcgsc.ca>
2020-02-11 13:15:29 -0800

    Don't block page load for dashboard api calls

--- app/components/report-listing/reports.module.js
+++ app/components/report-listing/reports.module.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 import reportsTable from './reports-table';
 
 export default angular.module('genomic', [])
-  .component('reportsTable', react2angular(reportsTable, ['rowData', 'columnDefs'], ['$state']))
+  .component('reportsTable', react2angular(reportsTable, ['columnDefs', 'isExternalMode'], ['$state', 'ReportService']))
   .config(($stateProvider) => {
     'ngInject';
 
@@ -14,41 +14,6 @@ export default angular.module('genomic', [])
         url: '/reports',
         component: 'reportsTable',
         resolve: {
-          rowData: ['ReportService', 'isExternalMode',
-            async (ReportService, isExternalMode) => {
-              const opts = {};
-
-              opts.all = true;
-              opts.states = 'ready,active,presented,uploaded,signedoff,archived';
-
-              if (isExternalMode) {
-                opts.states = 'presented,archived';
-              }
-              let { reports } = await ReportService.allFiltered(opts);
-
-              // Remove Dustin's test reports that are missing the patient info section
-              reports = reports.filter(r => r.patientInformation);
-
-              return reports.map((report) => {
-                const [analyst] = report.users
-                  .filter(u => u.role === 'analyst' && !u.deletedAt)
-                  .map(u => u.user);
-                
-                return {
-                  patientID: report.pog.POGID,
-                  analysisBiopsy: report.analysis.analysis_biopsy,
-                  reportType: report.type === 'genomic' ? 'Genomic' : 'Targeted Gene',
-                  state: report.state,
-                  caseType: report.patientInformation.caseType,
-                  project: report.pog.projects.map(project => project.name).sort().join(', '),
-                  physician: report.patientInformation.physician,
-                  analyst: analyst ? `${analyst.firstName} ${analyst.lastName}` : null,
-                  tumourType: report.patientInformation.tumourType,
-                  reportID: report.ident,
-                  date: report.createdAt,
-                };
-              });
-            }],
           columnDefs: ['isExternalMode', async (isExternalMode) => {
             // enable natural sorting for the Patient ID column and make it the default
             const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' });
