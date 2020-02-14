0ce29fe99dabda657eeed28e6cf714846f0d3315
Anna Davies <adavies@bcgsc.ca>
2020-02-14 10:57:59 -0800

    Add lazy loading to admin routes

--- app/views/admin/admin.module.js
+++ app/views/admin/admin.module.js
@@ -1,54 +1,23 @@
 import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
 import AdminComponent from './admin.component';
-import UsersModule from './users/users.module';
-import ProjectModule from './projects/projects.module';
-import GroupsModule from './groups/groups.module';
+import UsersComponent from './users/users.component';
+import ProjectsComponent from './projects/projects.component';
+import GroupsComponent from './groups/groups.component';
+import lazy from './lazy';
 
 angular.module('admin', [
   uiRouter,
-  UsersModule,
-  ProjectModule,
-  GroupsModule,
 ]);
 
 export default angular.module('admin')
   .component('admin', AdminComponent)
+  .component('users', UsersComponent)
+  .component('projects', ProjectsComponent)
+  .component('groups', GroupsComponent)
   .config(($stateProvider) => {
     'ngInject';
 
-    $stateProvider
-      .state('root.admin', {
-        url: '/admin',
-        component: 'admin',
-        resolve: {
-          users: ['UserService', UserService => UserService.all()],
-          groups: ['GroupService', GroupService => GroupService.all()],
-          projects: ['ProjectService', ProjectService => ProjectService.all({ admin: true })],
-          /* eslint-disable no-shadow */
-          user: ['UserService', '$state', async (UserService, $state) => {
-            try {
-              const resp = await UserService.me();
-              return resp;
-            } catch (err) {
-              $state.go('public.login');
-              return err;
-            }
-          }],
-          /* eslint-disable no-shadow */
-          isAdmin: ['UserService', '$state', async (UserService, $state) => {
-            try {
-              const resp = await UserService.isAdmin();
-              if (!resp) {
-                $state.go('root.reportlisting.reports');
-              }
-              return resp;
-            } catch (err) {
-              $state.go('public.login');
-              return err;
-            }
-          }],
-        },
-      });
+    Object.values(lazy).forEach(state => $stateProvider.state(state));
   })
   .name;
--- app/views/views.module.js
+++ app/views/views.module.js
@@ -2,16 +2,15 @@ import angular from 'angular';
 import ReportListingModule from './report-listing/report-listing.module';
 import ReportModule from './report/report.module';
 import PublicModule from './public/public.module';
-import AdminModule from './admin/admin.module';
 import LayoutModule from './layout/layout.module';
 import PrintModule from './print/print.module';
 import eagerGermlineStates from './germline/eager';
+import eagerAdminStates from './admin/eager';
 
 angular.module('root.views', [
   ReportListingModule,
   ReportModule,
   PublicModule,
-  AdminModule,
   LayoutModule,
   PrintModule,
 ]);
@@ -22,5 +21,6 @@ export default angular.module('root.views')
     'ngInject';
 
     Object.values(eagerGermlineStates).forEach(state => $stateProvider.state(state));
+    Object.values(eagerAdminStates).forEach(state => $stateProvider.state(state));
   })
   .name;
