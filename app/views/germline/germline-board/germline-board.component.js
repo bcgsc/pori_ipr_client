bd447427bf89fc49b6bac68ea6f6dcbdfa9d665a
Jacky Li <jacli@bcgsc.ca>
2020-03-12 15:58:28 -0700

    Update views/germline-report Update genomic-report.pug to use patientID Remove POG from small-mutations.component

--- app/views/germline/germline-board/germline-board.component.js
+++ app/views/germline/germline-board/germline-board.component.js
@@ -32,55 +32,54 @@ class GermlineBoardComponent {
 
   clearSearch() {
     this.focusSearch = false;
-    
+
     const filterCache = this.filter.search;
-    
+
     this.filter.search = null;
     if (filterCache !== undefined) {
       this.refreshReports();
     }
   }
-  
+
   displaySearch() {
     this.showSearch = true;
     this.focusSearch = true;
   }
-  
+
   /* eslint-disable class-methods-use-this */
   hasReview(report, type) {
     return report.reviews.find(review => review.type === type);
   }
-  
+
   async unsetExported(report) {
     try {
       if (report.exported === false) {
         return;
       }
-      
+
       report.exported = false;
-      
       const reportCache = angular.copy(report);
       reportCache.biofx_assigned = report.biofx_assigned.ident;
-      
-      const result = await this.GermlineService.updateReport(reportCache.analysis.pog.POGID,
-        reportCache.analysis.analysis_biopsy, reportCache.ident, reportCache);
+
+      const result = await this.GermlineService.updateReport(reportCache.patientId,
+        reportCache.biopsyName, reportCache.ident, reportCache);
       const i = this.reports.findIndex(rep => rep.ident === reportCache.ident);
-      
+
       this.reports[i] = result;
     } catch (err) {
       report.exported = true;
       this.$mdToast.showSimple('Failed to update report exported status.');
     }
   }
-  
-  
+
+
   /**
    * Update search criteria and trigger reload
    */
   search() {
     this.refreshReports();
   }
-  
+
   /**
    * Reload tracking data from API
    *
@@ -97,21 +96,21 @@ class GermlineBoardComponent {
       limit: this.paginate.limit,
       search: this.filter.search,
     };
-    
+
     const reports = await this.GermlineService.getAllReports(opts);
     this.paginate.total = reports.total;
     this.reports = reports.reports;
     this.loading = false;
     this.$scope.$digest();
   }
-  
+
   // Trigger download pipe
   async getExport() {
     try {
       const token = await this.GermlineService.getFlashToken();
       // Open a window for the user with the special url
       this.$window.open(`${CONFIG.ENDPOINTS.API}/export/germline_small_mutation/batch/download?reviews=biofx,projects&flash_token=${token.token}`, '_blank');
-      
+
       this.$timeout(() => {
         this.refreshReports();
       }, 500);
--- app/views/germline/germline-report/germline-report.component.js
+++ app/views/germline/germline-report/germline-report.component.js
@@ -237,7 +237,7 @@ class GermlineReportComponent {
 
     await Promise.all(
       this.report.variants.map(variant => this.GermlineService.updateVariant(
-        this.report.analysis.pog.POGID,
+        this.report.patientId,
         this.report.biopsyName,
         this.report.ident,
         variant.ident,
@@ -255,7 +255,7 @@ class GermlineReportComponent {
 
     try {
       const review = await this.GermlineService.addReview(
-        this.report.analysis.pog.POGID,
+        this.report.patientId,
         this.report.biopsyName,
         this.report.ident,
         data,
@@ -273,7 +273,7 @@ class GermlineReportComponent {
 
     try {
       const result = await this.GermlineService.updateVariant(
-        this.report.analysis.pog.POGID,
+        this.report.patientId,
         this.report.biopsyName,
         this.report.ident,
         variant.ident,
@@ -291,6 +291,7 @@ class GermlineReportComponent {
   async removeReview(review) {
     try {
       await this.GermlineService.removeReview(
+        this.report.patientId,
         this.report.biopsyName,
         this.report.ident,
         review.ident,
@@ -319,7 +320,7 @@ class GermlineReportComponent {
         return;
       }
       await this.GermlineService.deleteReport(
-        this.report.analysis.pog.POGID,
+        this.report.patientId,
         this.report.biopsyName,
         this.report.ident,
       );
