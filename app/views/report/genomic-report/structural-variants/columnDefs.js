3200a24e216af90a7f03b9a48ef903b179080683
Anna Davies <adavies@bcgsc.ca>
2020-03-27 16:10:18 -0700

    Update column defs to include action cell and gene viewer

--- app/views/report/genomic-report/structural-variants/columnDefs.js
+++ app/views/report/genomic-report/structural-variants/columnDefs.js
@@ -1,21 +1,4 @@
-import React from 'react';
-import { Launch } from '@material-ui/icons';
-import { IconButton } from '@material-ui/core';
-
 const columnDefs = [{
-  headerName: 'Image',
-  cellRendererFramework: function cellRendererFramework(params) {
-    if (params.data.svg) {
-      return (
-        <IconButton size="small">
-          <Launch size="small" />
-        </IconButton>
-      );
-    }
-    return null;
-  },
-  hide: false,
-}, {
   headerName: 'Genes 5`::3`',
   colId: 'genes',
   valueGetter: params => `${params.data.gene1.name} :: ${params.data.gene2.name}`,
@@ -54,6 +37,10 @@ const columnDefs = [{
   colId: 'tcgaPerc',
   valueGetter: params => `${params.data.gene1.expressionVariants.tcgaPerc}/${params.data.gene2.expressionVariants.tcgaPerc}`,
   hide: false,
+}, {
+  headerName: 'Actions',
+  cellRenderer: 'ActionCellRenderer',
+  pinned: 'right',
 }];
 
 export const setHeaderName = (header, colId) => {
