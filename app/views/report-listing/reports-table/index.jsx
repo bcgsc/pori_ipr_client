3195f7592a01f4daf04985eed2490dd025dbb80c
Anna Davies <adavies@bcgsc.ca>
2020-04-09 11:35:25 -0700

    Move ag-grid style imports to root

--- app/views/report-listing/reports-table/index.jsx
+++ app/views/report-listing/reports-table/index.jsx
@@ -2,9 +2,6 @@ import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { AgGridReact } from 'ag-grid-react';
 
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
-
 import './index.scss';
 
 /**
--- app/views/report/genomic-report/therapeutic/components/PrintTables/index.jsx
+++ app/views/report/genomic-report/therapeutic/components/PrintTables/index.jsx
@@ -3,9 +3,6 @@ import PropTypes from 'prop-types';
 import { AgGridReact } from 'ag-grid-react';
 import omit from 'lodash.omit';
 
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
-
 import './index.scss';
 
 /**
