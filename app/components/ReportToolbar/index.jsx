003bb9828a3d1522d4f675431028fe71667410f9
Anna Davies <adavies@bcgsc.ca>
2020-05-15 16:59:56 -0700

    Add react report toolbar

---
+++ app/components/ReportToolbar/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Typography,
+} from '@material-ui/core';
+
+import './index.scss';
+
+function ReportToolbar(props) {
+  const {
+    diagnosis,
+    patientId,
+    type,
+    state,
+  } = props;
+
+  return (
+    <span className="report__header">
+      <Typography display="inline" variant="h6">
+        {diagnosis}
+      </Typography>
+      <Typography display="inline" variant="h4">
+        {patientId}
+        {` ${type} Report`}
+      </Typography>
+      <Typography display="inline" variant="h6">
+        {state}
+      </Typography>
+      <span>
+        {' '}
+      </span>
+    </span>
+  );
+}
+
+ReportToolbar.propTypes = {
+  diagnosis: PropTypes.string,
+  patientId: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+};
+
+ReportToolbar.defaultProps = {
+  diagnosis: '',
+};
+
+export default ReportToolbar;
