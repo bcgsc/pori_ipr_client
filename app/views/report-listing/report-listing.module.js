8eab03125403015ab4e3dba9a064bc4a694a178c
Anna Davies <adavies@bcgsc.ca>
2020-05-27 10:25:27 -0700

    Remove old permission check to view reports

--- app/views/report-listing/report-listing.module.js
+++ app/views/report-listing/report-listing.module.js
@@ -10,17 +10,6 @@ export default angular.module('reportlisting', [
     $stateProvider
       .state('root.reportlisting', {
         abstract: true,
-        resolve: {
-          permission: ['$state', '$mdToast', 'AclService',
-            async ($state, $mdToast, AclService) => {
-              if (!(await AclService.checkAction('report.view'))) {
-                $mdToast.showSimple('You do not have permissions to view reports');
-                $state.go('public.login');
-                return false;
-              }
-              return true;
-            }],
-        },
       });
   })
   .name;
