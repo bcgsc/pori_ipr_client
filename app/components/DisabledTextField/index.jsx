a34e9da4f4ac03ff9d2268300ace07ea78618de5
Anna Davies <adavies@bcgsc.ca>
2020-07-17 17:34:30 -0700

    Create DisabledTextField for key value display

---
+++ app/components/DisabledTextField/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  TextField,
+} from '@material-ui/core';
+
+import './index.scss';
+
+const DisabledTextField = (props) => {
+  const {
+    label,
+    disableUnderline,
+    children,
+    ...rest
+  } = props;
+
+  return (
+    <TextField
+      label={label}
+      value={children === null ? ' ' : children}
+      classes={{ root: 'text-field' }}
+      InputProps={{ disableUnderline }}
+      disabled
+      {...rest}
+    />
+  );
+};
+
+DisabledTextField.propTypes = {
+  label: PropTypes.string,
+  disableUnderline: PropTypes.bool,
+  children: PropTypes.string,
+};
+
+DisabledTextField.defaultProps = {
+  label: '',
+  disableUnderline: false,
+  children: '',
+};
+
+export default DisabledTextField;
