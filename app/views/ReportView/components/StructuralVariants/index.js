ba42830120d36b420bfdffc35a9844a1be9b0f71
Caralyn Reisle <creisle@bcgsc.ca>
2020-08-14 13:42:16 -0700

    Update SV standard field names

--- app/views/ReportView/components/StructuralVariants/index.js
+++ app/views/ReportView/components/StructuralVariants/index.js
@@ -1,14 +1,12 @@
 import { angular2react } from 'angular2react';
 import { $rootScope } from 'ngimport';
 
-import MutationSummaryService from '@/services/reports/mutation-burden.service';
 import MavisService from '@/services/reports/mavis.service';
 import StructuralVariantsService from '@/services/reports/structural-variants.service';
-import ImageService from '@/services/reports/image.service';
 import lazyInjector from '@/lazyInjector';
 
 import template from './structural-variants.pug';
-import columnDefs, { setHeaderName } from './columnDefs';
+import columnDefs from './columnDefs';
 import './index.scss';
 
 const bindings = {
@@ -35,110 +33,21 @@ class StructuralVariants {
   async $onChanges(changes) {
     if (changes.report && changes.report.currentValue) {
       const [
-        images,
-        mutationSummary,
         structuralVariants,
-        mutationSummaryImages,
         mavisSummary,
       ] = await Promise.all([
-        ImageService.get(
-          this.report.ident,
-          'mutation_summary.barplot_sv,mutation_summary.density_plot_sv,circosSv.genome,circosSv.transcriptome',
-        ),
-        MutationSummaryService.get(this.report.ident),
         StructuralVariantsService.all(this.report.ident),
-        ImageService.mutationSummary(this.report.ident),
         MavisService.all(this.report.ident),
       ]);
 
-      this.images = images;
-      this.mutationSummary = mutationSummary;
       this.structuralVariants = structuralVariants;
-      this.mutationSummaryImages = mutationSummaryImages;
       this.mavisSummary = mavisSummary;
 
-      setHeaderName(`${this.report.tumourAnalysis.diseaseExpressionComparator || ''} %ile`, 'tcgaPerc');
-      setHeaderName(`Fold Change vs ${this.report.tumourAnalysis.normalExpressionComparator}`, 'foldChange');
       this.processSvs(this.structuralVariants);
-      this.pickComparator();
-      this.processMutationSummaryImages(this.mutationSummaryImages);
       this.loading = false;
       $rootScope.$digest();
     }
   }
-
-  pickComparator() {
-    let search = this.mutationSummary.find(entry => entry.comparator === this.report.tumourAnalysis.diseaseExpressionComparator);
-
-    if (!search) {
-      search = this.mutationSummary.find(entry => entry.comparator === 'average');
-    }
-
-    this.mutationSummary = search;
-  }
-
-
-  processMutationSummaryImages(images) {
-    const sorted = {
-      comparators: [],
-      sv: {
-        barplot: [],
-        densityPlot: [],
-      },
-      legend: {
-        sv: null,
-      },
-    };
-
-    Object.values(images).forEach((img) => {
-      if (!img.key.includes('sv')) {
-        return;
-      }
-
-      const pieces = img.key.split('.');
-      img.comparator = pieces[2] || null;
-      if (!img.comparator) {
-        // If no comparator found in image, likely legacy and use report setting.
-        img.comparator = this.report.tumourAnalysis.diseaseExpressionComparator || '';
-      }
-
-      if (img.comparator.toLowerCase()
-        && !sorted.comparators.some(comp => comp.name === img.comparator.toLowerCase())) {
-        sorted.comparators.push({ name: img.comparator.toLowerCase(), visible: false });
-      }
-
-      if (pieces[1].includes('barplot_sv') || pieces[1] === 'bar_sv') {
-        sorted.sv.barplot.push(img);
-      }
-      if (pieces[1].includes('density') || pieces[1] === 'sv') {
-        sorted.sv.densityPlot.push(img);
-      }
-      if (pieces[1].includes('legend_sv')) {
-        sorted.legend.sv = img;
-      }
-    });
-    this.mutationSummaryImages = sorted;
-  }
-
-  /**
-   * Retrieve specific mutation summary image
-   *
-   * @param {string} graph - The type of graph image to be retrieved (barplot, density graph, legend)
-   * @param {string} type - The type of analysis (snv, indel, sv)
-   * @param {string} comparator - OPTIONAL The comparator to be picked
-   *
-   * @returns
-   */
-  getMutationSummaryImage(graph, type, comparator = null) {
-    if (comparator === null) {
-      const img = this.mutationSummaryImages[type][graph];
-      if (img && img.length === 1) {
-        return img[0];
-      }
-    }
-    return this.mutationSummaryImages[type][graph].find(entry => (entry.comparator.toLowerCase() === comparator.toLowerCase()));
-  }
-
 
   processSvs(structVars) {
     const svs = {
