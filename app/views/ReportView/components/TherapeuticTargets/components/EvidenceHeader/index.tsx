cd870e40bd49e821bb76a3d56f3fe0fb428c7c74
Anna Davies <adavies@bcgsc.ca>
2020-12-09 15:30:12 -0800

    Add EvidenceHeader and InfoDialog

---
+++ app/views/ReportView/components/TherapeuticTargets/components/EvidenceHeader/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useCallback } from 'react';
+import { IconButton } from '@material-ui/core';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
+
+import InfoDialog from '../InfoDialog';
+
+const EvidenceHeader = (params) => {
+  const {
+    displayName,
+  } = params;
+
+  const [showDialog, setShowDialog] = useState<boolean>(false);
+
+  const handleClick = useCallback(() => {
+    if (!showDialog) {
+      setShowDialog(true);
+    }
+  }, [showDialog]);
+
+  const handleClose = () => {
+    setShowDialog(false);
+  };
+
+  return (
+    <span>
+      {displayName}
+      <IconButton size="small" onClick={handleClick}>
+        <HelpOutlineIcon />
+      </IconButton>
+      <InfoDialog
+        isOpen={showDialog}
+        onClose={handleClose}
+      />
+    </span>
+  );
+};
+
+export default EvidenceHeader;
---
+++ app/views/ReportView/components/TherapeuticTargets/components/InfoDialog/index.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@material-ui/core';
+
+import api from '../../../../../../services/api';
+
+import './index.scss';
+
+const InfoDialog = ({ isOpen, onClose }) => {
+  const [evidenceLevels, setEvidenceLevels] = useState([]);
+
+  useEffect(() => {
+    if (isOpen) {
+      const getData = async () => {
+        const evidenceLevelsResp = await api.get('/graphkb/evidenceLevels', {}).request();
+        if (evidenceLevelsResp) {
+          setEvidenceLevels(evidenceLevelsResp.result);
+        }
+      };
+      getData();
+    }
+  }, [isOpen]);
+
+  return (
+    <Dialog open={isOpen} onClose={onClose} maxWidth="sm" fullWidth className="edit-dialog">
+      <DialogTitle disableTypography>
+        <Typography variant="h3">
+          Evidence Levels
+        </Typography>
+      </DialogTitle>
+      <DialogContent>
+        {Boolean(evidenceLevels.length) && (
+          <>
+            {evidenceLevels.map(({ displayName, description }) => (
+              <div key={displayName} className="info__group">
+                <Typography variant="h5">
+                  {displayName}  
+                </Typography>
+                <Typography variant="body2">
+                  {description}  
+                </Typography>
+              </div>
+            ))}
+          </>
+        )}
+      </DialogContent>
+      <DialogActions>
+        <Button color="secondary" onClick={onClose}>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default InfoDialog;
