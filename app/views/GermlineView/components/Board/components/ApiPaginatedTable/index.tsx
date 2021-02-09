7d0f38f4dfa0a5a9590b576d1b664e5de01c48a8
Anna Davies <adavies@bcgsc.ca>
2021-02-08 16:17:32 -0800

    Add download export button

--- app/views/GermlineView/components/Board/components/ApiPaginatedTable/index.tsx
+++ app/views/GermlineView/components/Board/components/ApiPaginatedTable/index.tsx
@@ -1,8 +1,15 @@
 import React, { useCallback } from 'react';
 import { AgGridReact } from '@ag-grid-community/react';
 import { useHistory } from 'react-router-dom';
-import useGrid from '@/components/hooks/useGrid';
+import { useSnackbar } from 'notistack';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import {
+  Typography,
+  IconButton,
+} from '@material-ui/core';
 
+import germlineDownload from '@/services/reports/germline';
+import useGrid from '@/components/hooks/useGrid';
 import PaginationPanel from './components/PaginationPanel';
 import CheckboxCell from './components/CheckboxCell';
 import ReviewFilter from './components/ReviewFilter';
@@ -22,6 +29,7 @@ const ApiPaginatedTable = ({
 }: ApiPaginatedTableProps): JSX.Element => {
   const { gridApi, colApi, onGridReady } = useGrid();
   const history = useHistory();
+  const snackbar = useSnackbar();
 
   const onFirstDataRendered = useCallback(() => {
     const visibleColumnIds = colApi.getAllColumns()
@@ -41,8 +49,41 @@ const ApiPaginatedTable = ({
     }
   }, [gridApi, history]);
 
+  const handleExport = useCallback(async () => {
+    try {
+      const { filename, blob } = await germlineDownload();
+      const url = URL.createObjectURL(blob);
+
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = filename;
+
+      const clickHandler = () => {
+        setTimeout(() => {
+          URL.revokeObjectURL(url);
+          a.removeEventListener('click', clickHandler);
+        }, 150);
+      };
+
+      a.addEventListener('click', clickHandler, false);
+
+      a.click();
+    } catch (err) {
+      snackbar.enqueueSnackbar(`Download error: ${err}`);
+    }
+  }, [snackbar]);
+
   return (
     <div className="ag-theme-material paginated-table__container">
+      <div className="paginated-table__header">
+        <Typography variant="h3">Germline Reports</Typography>
+        <div className="paginated-table__action">
+          <Typography display="inline">Download Export</Typography>
+          <IconButton onClick={handleExport}>
+            <GetAppIcon />
+          </IconButton>
+        </div>
+      </div>
       <AgGridReact
         rowData={rowData}
         columnDefs={columnDefs}
