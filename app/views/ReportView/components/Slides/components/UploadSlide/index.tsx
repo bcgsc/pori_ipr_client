d65d7a460022a8abb9d0b28d209eece0ef5a5e83
Anna Davies <adavies@bcgsc.ca>
2021-04-06 11:58:41 -0700

    Add carousel and slide upload/delete logic

--- app/views/ReportView/components/Slides/components/UploadSlide/index.tsx
+++ app/views/ReportView/components/Slides/components/UploadSlide/index.tsx
@@ -1,35 +1,91 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback, useContext } from 'react';
 import {
   Typography,
   Button,
   TextField,
-  Input,
 } from '@material-ui/core';
+import { useSnackbar } from 'notistack';
 
 import api from '@/services/api';
+import ReportContext from '@/components/ReportContext';
+import SlideType from '../../types';
 
-const UploadSlide = (): JSX.Element => {
+import './index.scss';
+
+type UploadSlideProps = {
+  onUpload: (newData: SlideType) => void;
+};
+
+const UploadSlide = ({
+  onUpload,
+}: UploadSlideProps): JSX.Element => {
+  const snackbar = useSnackbar();
+  const { report } = useContext(ReportContext);
+
   const [slideName, setSlideName] = useState('');
+  const [imageError, setImageError] = useState('');
 
   const handleSlideNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSlideName(event.target.value);
   };
 
+  const handleImageUpload = useCallback(async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    const [file] = files;
+    if (!file?.name?.match(/\.(jpg|jpeg|png|gif)$/)) {
+      setImageError('Please select a valid image (.jpg/.jpeg/.png/.gif)');
+      return;
+    }
+    setImageError('');
+
+    try {
+      const newSlide = new FormData();
+
+      newSlide.append('name', slideName);
+      newSlide.append('file', file);
+
+      const resp = await api.post(`/reports/${report.ident}/presentation/slide`, newSlide, {}, true).request();
+      snackbar.enqueueSnackbar('Slide uploaded successfully', { variant: 'success' });
+      onUpload(resp);
+    } catch (err) {
+      snackbar.enqueueSnackbar(`Error uploading slide: ${err}`, { variant: 'error' });
+    }
+  }, [onUpload, slideName, snackbar, report]);
+
   return (
-    <div>
+    <>
       <TextField
         className="text-field-fix"
-        helperText="Slide name must be entered before a file can be selected"
+        color="secondary"
+        helperText="A slide name must be entered before a file can be selected (max 10MB)"
         label="Slide name"
         onChange={handleSlideNameChange}
         value={slideName}
         variant="outlined"
       />
-      <Button color="secondary" variant="contained" component="label">
-        Upload
-        <input accept="image/*" type="file" hidden />
+      <Button
+        className="upload__button"
+        color="secondary"
+        component="label"
+        disabled={!slideName}
+        variant="outlined"
+      >
+        Select File
+        <input
+          accept=".png,.jpeg,.jpg,.gif"
+          hidden
+          onChange={handleImageUpload}
+          type="file"
+        />
       </Button>
-    </div>
+      {imageError && (
+        <Typography color="error" className="upload--error">
+          {imageError}
+        </Typography>
+      )}
+    </>
   );
 };
 
