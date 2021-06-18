765e83ac4fde052309759f321a09504f7f4e736b
Anna Davies <adavies@bcgsc.ca>
2021-06-18 12:21:13 -0700

    Change chunking options

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -101,8 +101,19 @@ module.exports = {
     // }),
   ],
   optimization: {
+    moduleIds: 'hashed',
     splitChunks: {
-      chunks: 'all',
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        // vendor chunk
+        vendor: {
+          // sync + async chunks
+          chunks: 'all',
+          // import file path containing node_modules
+          test: /node_modules/
+        }
+      }
     },
   },
   mode: 'development',
