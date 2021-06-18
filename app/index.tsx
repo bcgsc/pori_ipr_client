4c823a882104fbcaf5d730d4556dd10fb82152d4
Anna Davies <adavies@bcgsc.ca>
2021-06-18 15:27:17 -0700

    Change order back

--- app/index.tsx
+++ app/index.tsx
@@ -7,5 +7,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-registerServiceWorker();
 ReactDOM.render(<App />, document.getElementById('base'));
+registerServiceWorker();
