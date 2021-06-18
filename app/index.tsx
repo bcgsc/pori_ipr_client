7a52acfe644e72b1a8ba77d6fb6286b99eecab5c
Anna Davies <adavies@bcgsc.ca>
2021-06-18 12:31:01 -0700

    Change order of bootstrap

--- app/index.tsx
+++ app/index.tsx
@@ -7,5 +7,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('base'));
 registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('base'));
