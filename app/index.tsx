bdc2617c8faedb94a412cc0c0bec0a4d14aa425a
Anna Davies <adavies@bcgsc.ca>
2021-06-28 13:59:36 -0700

    Unregister SW

--- app/index.tsx
+++ app/index.tsx
@@ -5,5 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('base'));
+unregister();
