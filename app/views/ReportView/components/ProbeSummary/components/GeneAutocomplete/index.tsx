ea77a8a5d092f6447cbb3911d60d6faca59a401f
Anna Davies <adavies@bcgsc.ca>
2021-09-09 09:10:51 -0700

    Add gene autocomplete component

---
+++ app/views/ReportView/components/ProbeSummary/components/GeneAutocomplete/index.tsx
@@ -0,0 +1,80 @@
+import React, {
+  useState, useEffect, useContext, useCallback,
+} from 'react';
+import { Autocomplete } from '@material-ui/lab';
+import {
+  TextField,
+} from '@material-ui/core';
+
+import api from '@/services/api';
+import ReportContext from '@/context/ReportContext';
+import { GeneType } from '@/common';
+
+type GeneAutocompleteProps = {
+  defaultValue: GeneType | null;
+  onChange: (newData: GeneType) => void;
+};
+
+const GeneAutocomplete = ({
+  defaultValue,
+  onChange,
+}: GeneAutocompleteProps): JSX.Element => {
+  const { report } = useContext(ReportContext);
+
+  const [options, setOptions] = useState<GeneType[]>([]);
+  const [value, setValue] = useState<GeneType>();
+  const [genes, setGenes] = useState<GeneType[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const genesResp = await api.get(`/reports/${report.ident}/genes`).request();
+      setGenes(genesResp);
+      setOptions(genesResp);
+    };
+    getData();
+  }, [report]);
+
+  useEffect(() => {
+    if (defaultValue) {
+      setValue(defaultValue);
+    }
+  }, [defaultValue]);
+
+  const handleInputChange = async ({ target: { value: queryValue } }) => {
+    if (queryValue) {
+      setOptions(genes.filter((gene) => (
+        gene.name.toLowerCase().includes(queryValue.toLowerCase())
+      )));
+    } else {
+      setOptions(genes);
+    }
+  };
+
+  const handleAutocompleteChange = useCallback((event, val) => {
+    setValue(val);
+    onChange(val);
+  }, [onChange]);
+
+  return (
+    <Autocomplete
+      autoHighlight
+      classes={{ root: 'autocomplete', popper: 'autocomplete__popper' }}
+      onChange={handleAutocompleteChange}
+      options={options}
+      getOptionLabel={(option) => option.name || ''}
+      value={value || {}}
+      renderInput={(params) => (
+        <TextField
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...params}
+          label="Gene"
+          variant="outlined"
+          margin="normal"
+          onChange={handleInputChange}
+        />
+      )}
+    />
+  );
+};
+
+export default GeneAutocomplete;
