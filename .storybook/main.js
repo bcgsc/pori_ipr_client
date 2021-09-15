28f532411fe032ccd4ce22e4331f77f3b7a02196
Anna Davies <adavies@bcgsc.ca>
2021-09-15 10:43:15 -0700

    Change story locations, remove default

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../app/**/*.stories.(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
