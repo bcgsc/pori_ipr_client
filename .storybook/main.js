38c410b22f33e20390dc07616f4fdd7cfc06f97f
Anna Davies <adavies@bcgsc.ca>
2021-09-16 14:53:23 -0700

    Add stories to SignatureCard

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,18 @@
+const custom = require('../config/webpack/webpack.config');
+
 module.exports = {
-  "stories": [
-    "../app/**/*.stories.(js|jsx|ts|tsx)",
+  stories: [
+    "../app/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: { ...config.module, rules: custom('prod').module.rules },
+      resolve: custom('prod').resolve
+    };
+  },
 }
