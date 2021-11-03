769e0a06f3e19f443ccae4ffd75d8f57befebf15
Anna Davies <adavies@bcgsc.ca>
2021-11-03 10:02:02 -0700

    Remove v4 theme creation

--- app/App.tsx
+++ app/App.tsx
@@ -3,10 +3,7 @@
  */
 import { SnackbarProvider } from 'notistack';
 import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider, Theme, StyledEngineProvider, adaptV4Theme } from '@mui/material/styles';
-import createGenerateClassName from '@mui/styles/createGenerateClassName';
-import jssPreset from '@mui/styles/jssPreset';
-import { create } from 'jss';
+import { createTheme, ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ModuleRegistry } from '@ag-grid-community/core';
@@ -23,14 +20,7 @@ import '@ag-grid-community/core/dist/styles/ag-theme-material.min.css';
 import '@fontsource/roboto';
 import './styles/ag-grid.scss';
 
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
-
-
-const theme = createTheme(adaptV4Theme({
+const theme = createTheme({
   direction: 'ltr',
   palette: {
     primary: {
@@ -54,7 +44,6 @@ const theme = createTheme(adaptV4Theme({
     text: {
       primary: cssTheme.textPrimary,
       secondary: cssTheme.textSecondary,
-      hint: cssTheme.textHint,
       disabled: cssTheme.textDisabled,
     },
   },
@@ -69,36 +58,17 @@ const theme = createTheme(adaptV4Theme({
     h6: { fontSize: cssTheme.fontSizeH6 },
     subtitle1: { fontSize: cssTheme.fontSizeSubtitle1 },
   },
-  // Dialog title header bug: https://github.com/mui-org/material-ui/issues/16569
-  props: {
-    MuiDialogTitle: {
-      disableTypography: true,
-    },
-  },
-  overrides: {
-    MuiDialogTitle: {
-      root: {
-        fontSize: cssTheme.fontSizeH3,
-      },
-    },
+  components: {
     MuiCssBaseline: {
-      '@global': {
-        html: {
-          WebkitFontSmoothing: 'auto',
+      styleOverrides: {
+        '@global': {
+          html: {
+            WebkitFontSmoothing: 'auto',
+          },
         },
       },
     },
   },
-}));
-
-const generateClassName = createGenerateClassName({
-  productionPrefix: 'ipr',
-});
-
-const jss = create({
-  ...jssPreset(),
-  // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
-  insertionPoint: 'jss-insertion-point',
 });
 
 ModuleRegistry.registerModules([
