c6f29db4f9d2c1b452f5dee784bd506e4f254fc7
Anna Davies <adavies@bcgsc.ca>
2021-11-03 15:17:19 -0700

    Re-order plots, fix styling

--- app/views/ReportView/components/ExpressionCorrelation/index.tsx
+++ app/views/ReportView/components/ExpressionCorrelation/index.tsx
@@ -18,16 +18,18 @@ import PlotByKey from './components/PlotByKey';
 
 import './index.scss';
 
+const LEGACY_EXPRESSION_KEYS = [
+  { key: 'expression.chart', title: 'Expression Chart' },
+  { key: 'expression.legend', title: 'Expression Legend' },
+];
+
 const IMAGE_KEYS = [
   { key: 'scpPlot', title: 'SCOPE' },
 ];
 
-const TOP_EXPRESSION_KEYS = [
+const EXPRESSION_KEYS = [
   { key: 'expression.spearman.tcga', title: 'TCGA' },
   { key: 'expression.spearman.target', title: 'TARGET' },
-];
-
-const BOTTOM_EXPRESSION_KEYS = [
   { key: 'expression.spearman.hartwig', title: 'HARTWIG' },
   { key: 'expression.spearman.cser', title: 'CSER' },
   { key: 'expression.spearman.gtex', title: 'GTEX' },
@@ -51,8 +53,8 @@ const ExpressionCorrelation = ({
         try {
           const allKeys = [
             ...IMAGE_KEYS.map((group) => group.key),
-            ...TOP_EXPRESSION_KEYS.map((group) => group.key),
-            ...BOTTOM_EXPRESSION_KEYS.map((group) => group.key),
+            ...LEGACY_EXPRESSION_KEYS.map((group) => group.key),
+            ...EXPRESSION_KEYS.map((group) => group.key),
           ].join(',');
 
           const [plotData, subtypePlotData, pairwiseData] = await Promise.all([
@@ -77,6 +79,22 @@ const ExpressionCorrelation = ({
     }
   }, [report, setIsLoading]);
 
+  const sampleExpressionCharts = (keySet: { key: string, title: string }[]): JSX.Element => (
+    <>
+      {keySet.map((group) => (
+        <div key={group.key}>
+          <Typography variant="h3" align="center" className="expression-correlation__header">
+            {group.title}
+          </Typography>
+          <PlotByKey
+            accessor={group.key}
+            plots={plots}
+          />
+        </div>
+      ))}
+    </>
+  );
+
   return (
     <div className="expression-correlation">
       <DemoDescription>
@@ -88,75 +106,60 @@ const ExpressionCorrelation = ({
       </DemoDescription>
       {!isLoading && (
         <>
-          <Typography variant="h3">Sample-Sample Expression Correlation</Typography>
-          <div className="expression-correlation__expression-charts">
-            {TOP_EXPRESSION_KEYS.map((group) => (
-              <div key={group.key}>
-                <Typography variant="h3" align="center" className="expression-correlation__header">
-                  {group.title}
-                </Typography>
-                <PlotByKey
-                  accessor={group.key}
-                  plots={plots}
-                />
-              </div>
-            ))}
-          </div>
-          {/* This section should only appear if there's data */}
-          {Boolean(subtypePlots.length) && (
-            <>
-              <Typography variant="h3" className="expression-correlation__header">
-                Subtyping
-              </Typography>
-              <div className="expression-correlation__subtype">
-                {subtypePlots.map((plot) => (
-                  <Image
-                    key={plot.ident}
-                    image={plot}
-                    showTitle
-                    showCaption
-                  />
-                ))}
-              </div>
-            </>
-          )}
-          <Divider />
-          <Typography className="expression-correlation__title" variant="h3">
+          <Typography variant="h3">
             Pairwise Expression Correlation
           </Typography>
           <CorrelationPlot
             pairwiseExpression={pairwiseExpression}
           />
           <Divider />
-          {/* This section should only appear if there's data */}
-          {plots?.length && plots.find((plot) => plot.key === 'scpPlot') && (
-            <div className="expression-correlation__section">
-              <Typography className="expression-correlation__title" variant="h3">
-                SCOPE
-              </Typography>
-              <div className="expression-correlation__scp-plot">
-                <PlotByKey
-                  plots={plots}
-                  accessor="scpPlot"
-                />
-              </div>
+          <div className="expression-correlation__section">
+            <Typography variant="h3">Sample-Sample Expression Correlation</Typography>
+            <div className="expression-correlation__expression-charts">
+              {sampleExpressionCharts(plots.find((plot) => plot.key === 'expression.chart')
+                ? LEGACY_EXPRESSION_KEYS
+                : EXPRESSION_KEYS)
+              }
             </div>
-          )}
-          <Divider />
-          <Typography variant="h3">Additional Sample-Sample Expression Correlation</Typography>
-          <div className="expression-correlation__expression-charts">
-            {BOTTOM_EXPRESSION_KEYS.map((group) => (
-              <div key={group.key}>
-                <Typography variant="h3" align="center" className="expression-correlation__header">
-                  {group.title}
-                </Typography>
-                <PlotByKey
-                  accessor={group.key}
-                  plots={plots}
-                />
-              </div>
-            ))}
           </div>
+          {/* This section should only appear if there's data */}
+          {Boolean(plots?.length) && plots.find((plot) => plot.key === 'scpPlot') && (
+            <>
+              <Divider />
+              <div className="expression-correlation__section">
+                <Typography className="expression-correlation__title" variant="h3">
+                  SCOPE
+                </Typography>
+                <div className="expression-correlation__scp-plot">
+                  <PlotByKey
+                    plots={plots}
+                    accessor="scpPlot"
+                  />
+                </div>
+              </div>
+            </>
+          )}
+          {/* This section should only appear if there's data */}
+          {Boolean(subtypePlots.length) && (
+            <>
+              <Divider />
+              <div className="expression-correlation__section">
+                <Typography variant="h3" className="expression-correlation__header">
+                  Subtyping
+                </Typography>
+                <div className="expression-correlation__subtype">
+                  {subtypePlots.map((plot) => (
+                    <Image
+                      key={plot.ident}
+                      image={plot}
+                      showTitle
+                      showCaption
+                    />
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
         </>
       )}
     </div>
