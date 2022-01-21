157c8e51b8a148c803319f2d985184a236a2d5d2
Jacky Li <jacli@jacli01.phage.bcgsc.ca>
2022-01-20 16:56:21 -0800

    feat: add appendixEditor component DEVSU-1675

---
+++ app/views/ReportView/components/Appendices/components/AppendixEditor/index.tsx
@@ -0,0 +1,89 @@
+import React, {
+  useState, useCallback
+} from 'react';
+import ReactQuill, { ReactQuillProps } from 'react-quill';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@mui/material';
+import 'react-quill/dist/quill.snow.css';
+import './index.scss';
+
+const defaultQuillProps: ReactQuillProps = {
+  modules: {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      [{ 'script': 'sub' }, { 'script': 'super' }],
+      [{ 'indent': '-1' }, { 'indent': '+1' }],
+      [{ 'header': [1, 2, 3, false] }],
+      ['clean']
+    ],
+    clipboard: {
+      // https://github.com/zenoamaro/react-quill/issues/281
+      // https://stackoverflow.com/questions/63678128/how-to-prevent-react-quill-to-insert-a-new-line-before-list-when-re-loading-cont
+      matchVisual: false,
+    },
+  },
+  style: {
+    fontFamily: "Roboto",
+    width: "100%",
+  }
+};
+
+type AppendixEditorProps = {
+  appendixText: string;
+  isOpen: boolean;
+  // Returns null if nothing is edited
+  onClose: (editedText?: string) => void;
+  title?: string;
+};
+
+const AppendixEditor = ({
+  appendixText,
+  isOpen,
+  onClose,
+  title = 'Edit Appendix',
+}: AppendixEditorProps): JSX.Element => {
+  const [editedText, setEditedText] = useState('');
+  const [isDirty, setIsDirty] = useState(false);
+
+  const handleOnEdit = useCallback((nextValue, _delta, source) => {
+    if (!isDirty && source !== 'api') { setIsDirty(true) }
+    setEditedText(nextValue);
+  }, []);
+
+  const handleOnSave = useCallback(() => {
+    if (!isDirty) {
+      onClose(null);
+      return;
+    }
+    onClose(editedText);
+  }, [editedText]);
+
+  return (
+    <Dialog fullWidth maxWidth="lg" open={isOpen} onClose={() => onClose(null)}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <div className="AppendixEditor__container">
+          <ReactQuill
+            {...defaultQuillProps}
+            theme="snow"
+            defaultValue={appendixText}
+            onChange={handleOnEdit}
+          />
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => onClose()}>Close</Button>
+        <Button color="secondary" onClick={handleOnSave}>Save</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default AppendixEditor;
