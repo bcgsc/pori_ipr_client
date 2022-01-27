7bc279631f361fca5efcf534a2c3530d32dfdceb
Abbey Reisle <areisle@bcgsc.ca>
2022-01-27 12:59:33 -0800

    style: fix eslint config
    did not fix any of the linting errors found at this time
     to reduce the number of possible changes

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "extends": [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "./tsconfig.json"
+  },
+  "rules": {
+    "react/function-component-definition": "off",
+    "import/extensions": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/require-default-props": "off",
+    "import/prefer-default-export": "warn",
+    "max-len": "off",
+    "no-param-reassign": "warn",
+    "no-underscore-dangle": ["warn", { "allow": ["_env_"] }],
+    "no-restricted-syntax": [
+      "error",
+      "ForInStatement",
+      "LabeledStatement",
+      "WithStatement"
+    ],
+    "prefer-destructuring": ["error", { "object": true, "array": true }],
+  },
+  "settings": {
+    "import/resolver": {
+      typescript: {}
+    },
+  },
+}
