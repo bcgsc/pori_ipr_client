b3d46ccc8a066545a8d8bb2dc7079b473587138d
rpletz <rpletz@bcgsc.ca>
2022-02-02 17:50:12 -0800

    Add cancer predisposition table

---
+++ app/views/ReportView/components/CancerPredisposition/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+import api from '@/services/api';
+import DataTable from '@/components/DataTable';
+import ReportContext from '@/context/ReportContext';
+import withLoading, { WithLoadingInjectedProps } from '@/hoc/WithLoading';
+import snackbar from '@/services/SnackbarUtils';
+import columnDefs from './columnDefs';
+
+type CancerPredispositionProps = WithLoadingInjectedProps;
+
+const CancerPredisposition = ({
+  isLoading,
+  setIsLoading,
+}: CancerPredispositionProps): JSX.Element => {
+  const { report } = useContext(ReportContext);
+
+  const [variants, setVariants] = useState();
+
+  useEffect(() => {
+    if (report) {
+      const getData = async () => {
+        try {
+          const variantsResp = await api.get(
+            `/reports/${report.ident}/kb-matches?category=cancer predisposition`,
+          ).request();
+          setVariants(variantsResp);
+        } catch (err) {
+          snackbar.error(`Network error: ${err}`);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+      getData();
+    }
+  }, [report, setIsLoading]);
+
+  return (
+    <div>
+      {!isLoading && (
+        <DataTable
+          canViewDetails
+          columnDefs={columnDefs}
+          rowData={variants}
+          titleText="Known and Novel Cancer Predisposition Variants"
+        />
+      )}
+    </div>
+  );
+};
+
+export default withLoading(CancerPredisposition);
