dfb1b269780d49c75c80e7a89876cce6f00b17bc
rpletz <rpletz@bcgsc.ca>
2022-02-18 14:45:25 -0800

    Move getGeneProp to utils file

--- app/views/ReportView/components/PharmacoGenomicSummary/columnDefs.ts
+++ app/views/ReportView/components/PharmacoGenomicSummary/columnDefs.ts
@@ -1,12 +1,5 @@
 import ArrayCell from '@/components/DataTable/components/ArrayCellRenderer';
-
-const getGeneProp = (params, property) => {
-  const { data: { variant, variantType } } = params;
-  if (variantType === 'sv') {
-    return variant.gene1[property] || variant.gene2[property] || false;
-  }
-  return variant.gene[property] || false;
-};
+import getGeneProp from '@/utils/getGeneProp';
 
 const sampleColumnDefs = [
   {
