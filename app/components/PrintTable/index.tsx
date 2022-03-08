ab49b1d6cfb33780fb1e331e81e7a3674f451b13
Jacky Li <jacli@jacli01.phage.bcgsc.ca>
2022-03-07 18:19:20 -0800

    fix: better sorting for PrintTable
    - Sorted by columnDef and simplifies logic for sorting table header and data
    
    no-ticket

--- app/components/PrintTable/index.tsx
+++ app/components/PrintTable/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useMemo } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { ColDef } from '@ag-grid-community/core';
+import { ColDef, ValueGetterParams } from '@ag-grid-community/core';
 
 import './index.scss';
 
@@ -14,72 +15,87 @@ type PrintTableProps = {
   noRowsText?: string;
 };
 
+/**
+ * Table display for print view, due to unexpected results from AgGrid
+ * Note: ColumnDefs with valueGetters should be in function form
+ */
 const PrintTable = ({
   data = [],
   columnDefs = [],
   order = [],
   noRowsText = '',
 }: PrintTableProps): JSX.Element => {
-  const rowSortFunc = useCallback(([keyA], [keyB]): number => {
-    const colA = columnDefs.find((col) => col.colId === keyA);
-    const colB = columnDefs.find((col) => col.colId === keyB);
+  const sortedColDefs = useMemo(() => columnDefs
+    .filter((col) => (col.headerName && col.hide !== true && col.headerName !== 'Actions'))
+    .sort((columnA, columnB): number => {
+      let indexA;
+      let indexB;
 
-    let indexA;
-    let indexB;
+      if (order?.length > 0) {
+        indexA = order.findIndex((key) => key === columnA.headerName && columnA.colId);
+        indexB = order.findIndex((key) => key === columnB.headerName && columnB.colId);
+      } else {
+        indexA = columnDefs.findIndex((col) => col.headerName === columnA.headerName && columnA.colId);
+        indexB = columnDefs.findIndex((col) => col.headerName === columnB.headerName && columnB.colId);
+      }
 
-    if (order?.length > 0) {
-      indexA = order.findIndex((key) => key === colA?.headerName);
-      indexB = order.findIndex((key) => key === colB?.headerName);
-    } else {
-      indexA = columnDefs.findIndex((colD) => colD?.field === colA?.field);
-      indexB = columnDefs.findIndex((colD) => colD?.field === colB?.field);
+      // -1 would sort the column first. Put not found columns last instead
+      if (indexA === -1) {
+        indexA = columnDefs.length;
+      }
+      if (indexB === -1) {
+        indexB = columnDefs.length;
+      }
+
+      if (indexA < indexB) {
+        return -1;
+      }
+      if (indexA > indexB) {
+        return 1;
+      }
+      return 0;
+    }), [columnDefs, order]);
+
+  const sortedData = useMemo(() => {
+    let component = (
+      <tr>
+        <td className="table__none" colSpan={sortedColDefs.length}>
+          {`${noRowsText || 'No Rows To Show'}`}
+        </td>
+      </tr>
+    );
+
+    if (data.length > 0) {
+      const massagedData = [];
+      data.forEach((dataRow) => {
+        const row = [];
+        sortedColDefs.forEach((colD) => {
+          const columnIdentifier = colD.colId || colD.field;
+          let cellValue = dataRow[columnIdentifier];
+
+          if (colD.valueGetter && typeof colD.valueGetter === 'function') {
+            cellValue = colD.valueGetter({ data: dataRow } as ValueGetterParams);
+          }
+          row.push(cellValue);
+        });
+        massagedData.push(row);
+      });
+      component = (
+        <>
+          {
+            massagedData.map((row, rowIdx) => (
+              <tr key={rowIdx} className="table__row">
+                {/* eslint-disable-next-line react/no-array-index-key */}
+                {row.map((value, cellIdx) => (<td key={`${rowIdx}-${cellIdx}-${value}`}>{value}</td>))}
+              </tr>
+            ))
+          }
+        </>
+      );
     }
 
-    // -1 would sort the column first. Put not found columns last instead
-    if (indexA === -1) {
-      indexA = columnDefs.length;
-    }
-    if (indexB === -1) {
-      indexB = columnDefs.length;
-    }
-
-    if (indexA < indexB) {
-      return -1;
-    }
-    if (indexA > indexB) {
-      return 1;
-    }
-    return 0;
-  }, [columnDefs, order]);
-
-  const headerSortFunc = useCallback((columnA, columnB): number => {
-    let indexA;
-    let indexB;
-
-    if (order?.length > 0) {
-      indexA = order.findIndex((key) => key === columnA.headerName && columnA.colId);
-      indexB = order.findIndex((key) => key === columnB.headerName && columnB.colId);
-    } else {
-      indexA = columnDefs.findIndex((col) => col.headerName === columnA.headerName && columnA.colId);
-      indexB = columnDefs.findIndex((col) => col.headerName === columnB.headerName && columnB.colId);
-    }
-
-    // -1 would sort the column first. Put not found columns last instead
-    if (indexA === -1) {
-      indexA = columnDefs.length;
-    }
-    if (indexB === -1) {
-      indexB = columnDefs.length;
-    }
-
-    if (indexA < indexB) {
-      return -1;
-    }
-    if (indexA > indexB) {
-      return 1;
-    }
-    return 0;
-  }, [columnDefs, order]);
+    return component;
+  }, [sortedColDefs, noRowsText, data]);
 
   return (
     <div className="table-container">
@@ -87,36 +103,15 @@ const PrintTable = ({
         <table className="table">
           <thead className="table__header">
             <tr>
-              {columnDefs
-                .filter((col) => (col.headerName && col.headerName !== 'Actions'))
-                .sort(headerSortFunc)
-                .map((col) => (
-                  <th key={col.headerName}>
-                    {col.headerName}
-                  </th>
-                ))}
+              {sortedColDefs.map((col) => (
+                <th key={col.headerName}>
+                  {col.headerName}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            {data.length ? (
-              <>
-                {data.map((val, index) => (
-                  <tr key={index} className="table__row">
-                    {Object.entries(val).sort(rowSortFunc).map(([key, value]) => (
-                      <td key={key}>
-                        {value}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </>
-            ) : (
-              <tr>
-                <td className="table__none" colSpan={columnDefs.length}>
-                  {`${noRowsText || 'No Rows To Show'}`}
-                </td>
-              </tr>
-            )}
+            {sortedData}
           </tbody>
         </table>
       )}
