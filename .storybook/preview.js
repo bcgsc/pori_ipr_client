4fc73dc9b0b9cb6e299f7c082ed365c125d0f66a
Jacky Li <jacli@jacli01.phage.bcgsc.ca>
2022-07-27 15:43:51 -0700

    feat: add mui theme in storybook to better illustrate closer to how component will look in production no-ticket

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { addDecorator } from "@storybook/react";
+import { withThemes } from "@react-theming/storybook-addon";
+import theme from '../app/appTheme';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +13,11 @@ export const parameters = {
     },
   },
 }
+
+const providerFn = ({ theme, children }) => {
+  const serialTheme = JSON.parse(JSON.stringify(theme));
+  const muiTheme = createTheme(serialTheme);
+  return <ThemeProvider theme={muiTheme}>{children}</ThemeProvider>;
+};
+
+addDecorator(withThemes(null, [theme], { providerFn }));
