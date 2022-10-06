0ce2f2b8f4fa336d25ffa8c1f034b84e530aea23
rpletz <rpletz@bcgsc.ca>
2022-10-06 12:19:38 -0700

    Move reloads to inside else statements

--- app/views/ReportView/components/TherapeuticTargets/components/EditDialog/index.tsx
+++ app/views/ReportView/components/TherapeuticTargets/components/EditDialog/index.tsx
@@ -114,15 +114,14 @@ const EditDialog = ({
           combinedData,
           {},
         );
-        let returnedData;
         if (isSigned) {
           showConfirmDialog(putTherapeuticTargetsCall);
         } else {
-          returnedData = await putTherapeuticTargetsCall.request();
+          const returnedData = await putTherapeuticTargetsCall.request();
+          setNewData({ type: 'replace', payload: {} });
+          setIsDirty(false);
+          onClose(returnedData);
         }
-        setNewData({ type: 'replace', payload: {} });
-        setIsDirty(false);
-        onClose(returnedData);
       }
     } catch (err) {
       snackbar.error(`Error: ${err}`);
@@ -142,14 +141,14 @@ const EditDialog = ({
         showConfirmDialog(delTherapeuticTargets);
       } else {
         await delTherapeuticTargets.request();
+        onClose(null);
       }
-      onClose(null);
     } catch (err) {
       snackbar.error(`Error: ${err}`);
     } finally {
       setIsDeleting(false);
     }
-  }, [onClose, newData.ident, report.ident, isSigned]);
+  }, [onClose, newData.ident, report.ident, isSigned, showConfirmDialog]);
 
   const handleAutocompleteValueSelected = (selectedValue, typeName) => {
     setIsDirty(true);
