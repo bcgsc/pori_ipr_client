c986c8bc69d8d0e37e7768081b28940df1367705
Jacky Li <jacli@jacli01.phage.bcgsc.ca>
2022-12-07 20:28:37 -0800

    feat: update Germline/EditDialog options to expand on editable fields - use react hook form for form state
    DEVSU-1909

--- app/views/GermlineView/components/Report/components/EditDialog/index.tsx
+++ app/views/GermlineView/components/Report/components/EditDialog/index.tsx
@@ -1,5 +1,5 @@
 import React, {
-  useState, useCallback, useContext, useEffect,
+  useCallback, useContext,
 } from 'react';
 import {
   Button,
@@ -8,16 +8,27 @@ import {
   DialogContent,
   DialogActions,
   TextField,
+  MenuItem,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  FormLabel,
+  Select,
+  InputLabel,
 } from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
 
 import api from '@/services/api';
 import snackbar from '@/services/SnackbarUtils';
-import GermlineReportContext from '@/context/GermlineReportContext';
+import GermlineReportContext, { VariantType } from '@/context/GermlineReportContext';
+
+const CGL_REVIEW_RESULTS_OPTIONS = ['pathogenic', 'likely pathogenic', 'VUS', 'likely benign', 'benign'];
 
 type EditDialogProps = {
   isOpen: boolean;
   onClose: (patientHistory?: string, familyHistory?: string) => void;
-  rowData: Record<string, string>;
+  rowData: VariantType;
 };
 
 const EditDialog = ({
@@ -26,26 +37,20 @@ const EditDialog = ({
   rowData,
 }: EditDialogProps): JSX.Element => {
   const { report } = useContext(GermlineReportContext);
+  const {
+    handleSubmit,
+    control,
+    formState: { isDirty, dirtyFields },
+  } = useForm();
 
-  const [patientHistory, setPatientHistory] = useState('');
-  const [familyHistory, setFamilyHistory] = useState('');
-
-  useEffect(() => {
-    if (rowData) {
-      setPatientHistory(rowData.patientHistory);
-      setFamilyHistory(rowData.familyHistory);
-    }
-  }, [rowData]);
-
-  const handleSaveHistory = useCallback(async () => {
+  const saveHistory = useCallback(async (data) => {
     try {
       const updateFields: Record<string, unknown> = {};
-      if (patientHistory !== rowData.patientHistory) {
-        updateFields.patientHistory = patientHistory;
-      }
-      if (familyHistory !== rowData.familyHistory) {
-        updateFields.familyHistory = familyHistory;
-      }
+      Object.keys(data).forEach((key) => {
+        if (dirtyFields[key]) {
+          updateFields[key] = data[key];
+        }
+      });
 
       const newVariant = await api.put(
         `/germline-small-mutation-reports/${report.ident}/variants/${rowData.ident}`,
@@ -57,7 +62,7 @@ const EditDialog = ({
     } catch (err) {
       snackbar.error(`Error saving details: ${err}`);
     }
-  }, [familyHistory, onClose, patientHistory, report, rowData]);
+  }, [onClose, report, rowData, dirtyFields]);
 
   return (
     <Dialog
@@ -70,31 +75,132 @@ const EditDialog = ({
         Edit Row
       </DialogTitle>
       <DialogContent>
-        <TextField
-          fullWidth
-          label="Patient History"
-          margin="normal"
-          multiline
-          onChange={(event) => setPatientHistory(event.target.value)}
-          value={patientHistory}
-          variant="outlined"
+        <Controller
+          control={control}
+          name="patientHistory"
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              fullWidth
+              label="Patient History"
+              margin="normal"
+              multiline
+              onChange={onChange}
+              value={value}
+              defaultValue={rowData.patientHistory}
+              variant="outlined"
+            />
+          )}
         />
-        <TextField
-          fullWidth
-          label="Family History"
-          margin="normal"
-          multiline
-          onChange={(event) => setFamilyHistory(event.target.value)}
-          value={familyHistory}
-          variant="outlined"
+        <Controller
+          control={control}
+          name="familyHistory"
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              fullWidth
+              label="Family History"
+              margin="normal"
+              multiline
+              onChange={onChange}
+              value={value}
+              defaultValue={rowData.familyHistory}
+              variant="outlined"
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="cglReviewResult"
+          render={({ field: { onChange, value } }) => (
+            <FormControl fullWidth margin="normal">
+              <InputLabel id="cglReviewResult">CGL Review Result</InputLabel>
+              <Select
+                fullWidth
+                label="CGL Review Result"
+                labelId="cglReviewResult"
+                onChange={onChange}
+                value={value ?? ''}
+                defaultValue={rowData.cglReviewResult}
+                variant="outlined"
+              >
+                {CGL_REVIEW_RESULTS_OPTIONS.map((option) => <MenuItem key={option} value={option}>{option}</MenuItem>)}
+              </Select>
+            </FormControl>
+          )}
+        />
+        <Controller
+          control={control}
+          name="returnedToClinician"
+          render={({ field: { onChange, value } }) => (
+            <FormControl fullWidth margin="normal">
+              <FormLabel>Returned to Clinician</FormLabel>
+              <RadioGroup
+                onChange={onChange}
+                defaultValue={rowData.returnedToClinician}
+                value={value ?? null}
+              >
+                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="no" control={<Radio />} label="No" />
+              </RadioGroup>
+            </FormControl>
+          )}
+        />
+        <Controller
+          control={control}
+          name="referralHCP"
+          render={({ field: { onChange, value } }) => (
+            <FormControl fullWidth margin="normal">
+              <FormLabel>Referral HCP</FormLabel>
+              <RadioGroup
+                onChange={onChange}
+                defaultValue={rowData.referralHcp}
+                value={value ?? null}
+              >
+                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="no" control={<Radio />} label="No" />
+              </RadioGroup>
+            </FormControl>
+          )}
+        />
+        <Controller
+          control={control}
+          name="knownToHCP"
+          render={({ field: { onChange, value } }) => (
+            <FormControl fullWidth margin="normal">
+              <FormLabel>Known to HCP</FormLabel>
+              <RadioGroup
+                onChange={onChange}
+                defaultValue={rowData.knownToHcp}
+                value={value ?? null}
+              >
+                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="no" control={<Radio />} label="No" />
+              </RadioGroup>
+            </FormControl>
+          )}
+        />
+        <Controller
+          control={control}
+          name="reasonNoHcpReferral"
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              fullWidth
+              label="Reason no HCP referral"
+              margin="normal"
+              multiline
+              onChange={onChange}
+              value={value}
+              defaultValue={rowData.reasonNoHcpReferral}
+              variant="outlined"
+            />
+          )}
         />
       </DialogContent>
       <DialogActions>
         <Button onClick={() => onClose()}>Cancel</Button>
         <Button
           color="secondary"
-          disabled={patientHistory === rowData?.patientHistory && familyHistory === rowData?.familyHistory}
-          onClick={handleSaveHistory}
+          disabled={!isDirty}
+          onClick={handleSubmit(saveHistory)}
         >
           Save
         </Button>
