1aef58897557f436fd0dde64dfaddd005b9e773e
Jacky Li <jacli@jacli01.phage.bcgsc.ca>
2023-06-08 16:32:42 -0700

    feat: update all checkAccess usage for updated permission change DEVSU-1967

--- app/utils/checkAccess.ts
+++ app/utils/checkAccess.ts
@@ -1,10 +1,30 @@
+const ALL_ROLES = [
+  'admin',
+  'Analyst',
+  'BioApps',
+  'Bioinformatician',
+  'Biopsies',
+  'Biospecimen Core',
+  'Clinician',
+  'Collaborator',
+  'Demo',
+  'External Analyst',
+  'LIMS',
+  'manager',
+  'non-production access',
+  'Pipelines',
+  'Projects',
+  'Report Manager',
+  'Research',
+];
+
 /*
   Checks if a user is allowed access, given an allow and block list, with allow list taking precedence
 */
 const checkAccess = (
   groups: { ident: string, name: string }[],
   allowList: string[],
-  blockList: string[],
+  blockList: string[] = ALL_ROLES,
 ): boolean => {
   const groupNames = groups.map((group) => group.name.toLowerCase());
 
@@ -13,4 +33,8 @@ const checkAccess = (
   return isAllowed || !isBlocked;
 };
 
+export {
+  checkAccess,
+  ALL_ROLES,
+};
 export default checkAccess;
