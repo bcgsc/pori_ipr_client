3f30257a913a60392fdbb53d4aa55fd97fb63efb
Jacky Li <jacli@jacli01.phage.bcgsc.ca>
2023-06-09 15:47:46 -0700

    feat: multiple ArrayCellRenderer now shows a button DEVSU-1949

--- app/components/DataTable/components/ArrayCellRenderer/index.tsx
+++ app/components/DataTable/components/ArrayCellRenderer/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ICellRendererParams } from '@ag-grid-community/core';
-
+import {
+  Button, List, Popover, ListItem,
+} from '@mui/material';
+import { MoreHoriz } from '@mui/icons-material';
 import './index.scss';
 
 const urlRegex = /^(?:https?:\/\/)?(?:[\w-]+\.)+[a-z]{2,}(?:\/[\w\-\.\/]*)*$/i;
@@ -9,48 +12,84 @@ const urlRegex = /^(?:https?:\/\/)?(?:[\w-]+\.)+[a-z]{2,}(?:\/[\w\-\.\/]*)*$/i;
 const RenderArrayCell = (field: string, isLink: boolean): (cellParams: ICellRendererParams) => JSX.Element => {
   if (isLink) {
     return function ArrayCell({ data }: ICellRendererParams) {
-      if (Array.isArray(data[field])) {
-        const cellData = [...data[field]].sort();
+      const [anchorEl, setAnchorEl] = useState(null);
+      const [isOpen, setIsOpen] = useState(false);
+      const handleClose = () => {
+        setIsOpen(false);
+      };
+      const handleClick = useCallback((event) => {
+        setAnchorEl(event.currentTarget);
+        setIsOpen(true);
+      }, []);
 
-        const firstVal = cellData[0]?.replace(/(pmid:)|(#)/, '');
+      if (Array.isArray(data[field]) && data[field].length > 0) {
+        // Sort by PMID first
+        const cellData: string[] = [...data[field]];
 
-        let link = firstVal;
-        let validLink = false;
+        // Finds all pmid and # strings, removes them, and converts to integer, then sorts by lowest PMID first
+        const pmidArray = cellData.filter((item) => item.match(/(pmid:)|(#)/)).map((pmid) => {
+          const possibleNum = parseInt(pmid.replace(/(pmid:)|(#)/, ''), 10);
+          if (Number.isNaN(possibleNum)) {
+            return pmid;
+          }
+          return possibleNum;
+        }).sort();
+        const nonPmidArray = cellData.filter((item) => !item.match(/(pmid:)|(#)/));
 
-        // firstVal might be non-link
-        if (firstVal.match(/^\d+$/)) {
-          link = `https://ncbi.nlm.nih.gov/pubmed/${firstVal}`;
-          validLink = true;
-        } else if (urlRegex.test(firstVal)) {
-          validLink = true;
-        }
+        const linksComponent = pmidArray.map((pmid) => {
+          let link = null;
+          let isValidLink = false;
+          if ((/^\d+$/).test(String(pmid))) {
+            link = `https://ncbi.nlm.nih.gov/pubmed/${pmid}`;
+            isValidLink = true;
+          } else if (urlRegex.test(String(pmid))) {
+            link = pmid;
+            isValidLink = true;
+          }
 
-        let linkComponent = firstVal;
+          if (isValidLink) {
+            return (
+              <a
+                className="array-cell__link"
+                href={link}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {pmid}
+              </a>
+            );
+          }
+          return <span>{pmid}</span>;
+        }).concat(nonPmidArray.map((nonPmid) => <span>{nonPmid}</span>));
 
-        if (validLink) {
-          linkComponent = (
-            <a
-              className="array-cell__link"
-              href={link}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {firstVal}
-            </a>
+        if (linksComponent.length > 1) {
+          return (
+            <>
+              <Button endIcon={MoreHoriz} sx={{ padding: 0 }} onClick={handleClick}>Multiple</Button>
+              <Popover
+                open={isOpen}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center',
+                }}
+              >
+                <List>{linksComponent.map((c) => <ListItem>{c}</ListItem>)}</List>
+              </Popover>
+            </>
           );
         }
-
-        return (
-          <div>
-            {linkComponent}
-            {cellData.length > 1 && (
-              <>
-                …
-              </>
-            )}
-          </div>
-        );
+        if (linksComponent.length === 1 && pmidArray.length === 1) {
+          return linksComponent[0];
+        }
+        return <div>{linksComponent}</div>;
       }
+
       return (
         <div>
           {data[field]}
