eff4f5b2c20eb6a38b210349694131c94ba838e2
Jacky Li <jacli@jacli01.phage.bcgsc.ca>
2023-10-25 14:30:24 -0700

    fix: don't pollute KbMatchType with coalesced types DEVSU-2049

--- app/common.d.ts
+++ app/common.d.ts
@@ -128,7 +128,7 @@ type KbMatchType<T extends AnyVariantType = AnyVariantType> = {
   kbData: {
     inferred: boolean;
   } | null;
-  kbStatementId: string | string[];
+  kbStatementId: string;
   kbVariant: string;
   kbVariantId: string;
   matchedCancer: boolean;
