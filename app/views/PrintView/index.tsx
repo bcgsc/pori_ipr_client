0b1aa3aa222f56e9803c98a0044a9b387084bd1e
Jacky Li <jacli@jacli01.phage.bcgsc.ca>
2023-12-12 18:34:04 -0800

    feat: remove time from PDF name DEVSU-2126

--- app/views/PrintView/index.tsx
+++ app/views/PrintView/index.tsx
@@ -170,10 +170,6 @@ const Print = ({
           year: 'numeric',
           month: 'numeric',
           day: 'numeric',
-          hour: 'numeric',
-          minute: 'numeric',
-          second: 'numeric',
-          hour12: false,
         }).replace(', ', '-')}`;
         window.print();
         setIsPrintDialogShown(true);
