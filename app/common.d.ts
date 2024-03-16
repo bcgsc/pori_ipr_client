e176ab6163eb6ae1fda013ecf2222a03e6024489
Bill Nguyen <bnguyen@bcgsc.ca>
2024-03-15 17:26:56 -0700

    - DEVSU-2240 - Add pedsPercentage to ImmuneCellType

--- app/common.d.ts
+++ app/common.d.ts
@@ -314,6 +314,7 @@ type ImmuneType = {
   kbCategory: string | null;
   percentile: number | null;
   score: number | null;
+  pedsPercentile: number | null;
   pedsScore: number | null;
   pedsScoreComment: string | null;
 } & RecordDefaults;
