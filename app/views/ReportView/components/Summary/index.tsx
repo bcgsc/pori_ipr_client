c79c8024cc2a97c51b23429ed20b8e172f261adc
bnguyen-bcgsc <bnguyen@bcgsc.ca>
2024-05-15 14:18:15 -0700

    - Code lint

--- app/views/ReportView/components/Summary/index.tsx
+++ app/views/ReportView/components/Summary/index.tsx
@@ -15,7 +15,7 @@ const Summary = ({
   // TODO remove backup template name checks when data is updated in prod
   let summarySection;
   if (visibleSections) {
-    summarySection = visibleSections.find(element => ['summary-genomic', 'summary-tgr', 'summary-pcp', 'summary-probe'].includes(element));
+    summarySection = visibleSections.find((element) => ['summary-genomic', 'summary-tgr', 'summary-pcp', 'summary-probe'].includes(element));
   }
 
   if (summarySection === 'summary-probe' || templateName === 'probe') {
