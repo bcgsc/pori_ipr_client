c0a673482f1ad956ddfd8e83e21b53a457b860c7
bnguyen-bcgsc <bnguyen@bcgsc.ca>
2024-07-24 15:27:12 -0700

    - Move help text in search view to dialog

--- app/views/SearchView/index.tsx
+++ app/views/SearchView/index.tsx
@@ -4,17 +4,42 @@ import {
   TextField,
   Typography,
   Button,
+  Dialog,
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import HelpIcon from '@mui/icons-material/Help';
 import React, { useCallback, useEffect, useState } from 'react';
 
 const MIN_WORD_LENGTH = 3;
+
+type SearchHelpDialogProps = {
+  open: boolean;
+  onClose: () => void;
+}
+
+const SearchHelpDialog = ({open, onClose}: SearchHelpDialogProps) => {
+  const handleClose = () => {
+    onClose();
+  }
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <div className="help-dialog">
+        <Typography variant="subtitle2">
+          The matching threshold determines the cutoff of similarity between the key variant and the search results.
+          A value of 1 means a 100% exact match and vice versa. The default value is 0.8 if not specified.
+        </Typography>
+      </div>
+    </Dialog>
+  );
+}
 
 const SearchView = () => {
   const [variant, setVariant] = useState('');
   const [threshold, setThreshold] = useState('');
   const [variantErrorMessage, setVariantErrorMessage] = useState('');
   const [thresholdErrorMessage, setThresholdErrorMessage] = useState('');
+  const [helpDialogOpen, setHelpDialogOpen] = useState(false);
 
   // Calls submit function
   const handleSubmit = useCallback(() => {
@@ -77,6 +102,14 @@ const SearchView = () => {
     }
   }, [threshold]);
 
+  const handleHelpDialogOpen = () => {
+    setHelpDialogOpen(true);
+  }
+
+  const handleHelpDialogClose = () => {
+    setHelpDialogOpen(false);
+  }
+
   return (
     <div className="search">
       <div className="search__bar">
@@ -96,7 +129,7 @@ const SearchView = () => {
           />
         </div>
         <div className="search__threshold-input">
-          <Typography variant="subtitle1">Matching Threshold</Typography>
+          <Typography variant="subtitle1">Threshold</Typography>
           <TextField
             size="small"
             variant="outlined"
@@ -110,14 +143,13 @@ const SearchView = () => {
       </div>
       <div className="__button-container">
         <div className="button-holder">
-          <Typography
-            variant="subtitle2"
-            color="primary"
-            sx={{ maxWidth: '80%', marginRight: 4 }}
-          >
-            * Matching threshold determines the similarity between the key variant and the search results.
-            A value of 1 means a 100% exact match and vice versa. The default value is 0.8 if not specified.
-          </Typography>
+          <Button onClick={handleHelpDialogOpen} className="help-button">
+            <HelpIcon/>
+          </Button>
+          <SearchHelpDialog 
+            open={helpDialogOpen}
+            onClose={handleHelpDialogClose}
+          />
           <Button
             variant="contained"
             onClick={handleSubmit}
