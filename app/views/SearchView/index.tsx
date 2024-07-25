435e1ca4288cd04ae40f235d850bf64c94ca4170
Bill Nguyen <bnguyen@bcgsc.ca>
2024-07-25 09:53:32 -0700

    - Revamp look and feel and allow pressing enter to submit search

--- app/views/SearchView/index.tsx
+++ app/views/SearchView/index.tsx
@@ -3,45 +3,21 @@ import './index.scss';
 import {
   TextField,
   Typography,
-  Button,
-  Dialog,
   InputAdornment,
+  IconButton,
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import HelpIcon from '@mui/icons-material/Help';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import React, { useCallback, useEffect, useState } from 'react';
-import { borderRadius } from '@mui/system';
 
 const MIN_WORD_LENGTH = 3;
-
-type SearchHelpDialogProps = {
-  open: boolean;
-  onClose: () => void;
-}
-
-const SearchHelpDialog = ({open, onClose}: SearchHelpDialogProps) => {
-  const handleClose = () => {
-    onClose();
-  }
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <div className="help-dialog">
-        <Typography variant="subtitle2">
-          The matching threshold determines the cutoff of similarity between the key variant and the search results.
-          A value of 1 means a 100% exact match and vice versa. The default value is 0.8 if not specified.
-        </Typography>
-      </div>
-    </Dialog>
-  );
-}
 
 const SearchView = () => {
   const [variant, setVariant] = useState('');
   const [threshold, setThreshold] = useState('');
   const [variantErrorMessage, setVariantErrorMessage] = useState('');
   const [thresholdErrorMessage, setThresholdErrorMessage] = useState('');
-  const [helpDialogOpen, setHelpDialogOpen] = useState(false);
+  const ENTER_KEYCODE = 13;
 
   // Calls submit function
   const handleSubmit = useCallback(() => {
@@ -104,19 +80,12 @@ const SearchView = () => {
     }
   }, [threshold]);
 
-  const handleHelpDialogOpen = () => {
-    setHelpDialogOpen(true);
-  }
-
-  const handleHelpDialogClose = () => {
-    setHelpDialogOpen(false);
-  }
-
   return (
     <div className="search">
       <div className="search__bar">
         <div
           className="search__main"
+          onKeyUp={(event) => event.keyCode === ENTER_KEYCODE && handleSubmit()}
           role="textbox"
           tabIndex={0}
         >
@@ -134,20 +103,20 @@ const SearchView = () => {
                   <SearchIcon />
                 </InputAdornment>
               ),
-              sx: {borderRadius: '15px'}
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton color="primary" onClick={handleSubmit} disabled={!!variantErrorMessage || !!thresholdErrorMessage} type="submit">
+                    <ArrowCircleRightIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+              sx: { borderRadius: '15px' },
             }}
           />
         </div>
         <div className="search__threshold-input">
           <div className="search__threshold-input__label">
-            <Typography variant="subtitle1" sx={{width: "fit-content"}}>Threshold</Typography>
-            <Button onClick={handleHelpDialogOpen} className="help-button">
-              <HelpIcon/>
-            </Button>
-            <SearchHelpDialog 
-              open={helpDialogOpen}
-              onClose={handleHelpDialogClose}
-            />
+            <Typography variant="subtitle1" sx={{ width: 'fit-content' }}>Threshold</Typography>
           </div>
           <TextField
             size="small"
@@ -160,18 +129,11 @@ const SearchView = () => {
           />
         </div>
       </div>
-      <div className="__button-container">
-        <div className="button-holder">
-          <Button
-            variant="contained"
-            onClick={handleSubmit}
-            color="primary"
-            disabled={!!variantErrorMessage || !!thresholdErrorMessage}
-            size="large"
-          >
-            Search
-          </Button>
-        </div>
+      <div className="help-dialog">
+        <Typography variant="subtitle2" color="primary">
+          The matching threshold determines the cutoff of similarity between the key variant and the search results.
+          A value of 1 means a 100% exact match and vice versa. The default value is 0.8 if not specified.
+        </Typography>
       </div>
     </div>
   );
