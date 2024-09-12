7608e33046455a171c3dc1fe2c6640c5f3c8162b
Bill Nguyen <bnguyen@bcgsc.ca>
2024-09-12 16:39:13 -0700

    - DEVSU-2433 - Realign checkbox to add user to graphkb in add edit user dialog

--- app/views/AdminView/components/Users/components/AddEditUserDialog/index.tsx
+++ app/views/AdminView/components/Users/components/AddEditUserDialog/index.tsx
@@ -127,6 +127,7 @@ const AddEditUserDialog = ({
         }
       } catch (projectGroupErr) {
         snackbar.error('Failed to retrieve list of project and groups for current user.');
+        // eslint-disable-next-line no-console
         console.error(projectGroupErr);
       } finally {
         setIsDataLoading(false);
@@ -456,6 +457,7 @@ const AddEditUserDialog = ({
             <Checkbox checked={gkbAdd} onChange={handleCheckboxChange} />
           }
           disabled={Boolean(editData)}
+          sx={{ paddingLeft: '6px' }}
         />
         <AsyncButton isLoading={isApiCalling} color="inherit" onClick={() => onClose(null)}>
           Cancel
