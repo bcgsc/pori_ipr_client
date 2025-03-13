cd6fad24e8af3cff5228c197478f19be6c585532
bnguyen-bcgsc <bnguyen@bcgsc.ca>
2025-03-13 15:08:09 -0700

    - DEVSU-2591 - Fix issue where undefined properties in variant would crash filter table text search bar in kbmatches view

--- app/utils/getGeneProp.js
+++ app/utils/getGeneProp.js
@@ -1,9 +1,9 @@
 const getGeneProp = (params, property) => {
   const { data: { variant, variantType } } = params;
   if (variantType === 'sv') {
-    return variant.gene1?.[property] || variant.gene2?.[property] || false;
+    return variant?.gene1?.[property] || variant.gene2?.[property] || false;
   }
-  return variant.gene?.[property] || false;
+  return variant?.gene?.[property] || false;
 };
 
 export default getGeneProp;
