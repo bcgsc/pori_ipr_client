fa79640f943951eb37d5b448ed9e0b5d76691a22
Eleanor Lewis <elewis@bcgsc.ca>
2025-09-18 11:57:09 -0700

    update setting rapidtable from summary page

--- app/views/ReportView/components/RapidSummary/components/RapidVariantEditDialog/index.tsx
+++ app/views/ReportView/components/RapidSummary/components/RapidVariantEditDialog/index.tsx
@@ -315,14 +315,33 @@ const RapidVariantEditDialog = ({
         }
 
         if (fields.includes(FIELDS.kbMatches) && data?.kbMatches && editData?.kbMatches) {
+          // strip the context-related tag that has been added to the ident for coalescing
+          const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+          const variantIdent = uuidRegex.test(data.ident) ? data.ident : data.ident.split('-').slice(0, 5).join('-');
           noTableSet.forEach((ident) => {
-            calls.push(api.put(`/reports/${report.ident}/kb-matches/kb-matched-statements/${ident}`, {
-              kbData: { rapidReportTableTag: 'noTable' },
+            // calls.push(api.put(`/reports/${report.ident}/kb-matches/kb-matched-statements/${ident}`, {
+            //  kbData: { rapidReportTableTag: 'noTable' },
+            // }));
+            calls.push(api.post(`/reports/${report.ident}/variants/set-summary-table`, {
+              variantIdent,
+              variantType: data.variantType,
+              annotations: {
+                rapidReportTableTag: 'noTable',
+              },
+              kbStatementIds: [ident],
             }));
           });
           tableTypeSet.forEach((ident) => {
-            calls.push(api.put(`/reports/${report.ident}/kb-matches/kb-matched-statements/${ident}`, {
-              kbData: { rapidReportTableTag: rapidVariantTableType },
+            // calls.push(api.put(`/reports/${report.ident}/kb-matches/kb-matched-statements/${ident}`, {
+            //  kbData: { rapidReportTableTag: rapidVariantTableType },
+            // }));
+            calls.push(api.post(`/reports/${report.ident}/variants/set-summary-table`, {
+              variantIdent,
+              variantType: data.variantType,
+              annotations: {
+                rapidReportTableTag: rapidVariantTableType,
+              },
+              kbStatementIds: [ident],
             }));
           });
         }
